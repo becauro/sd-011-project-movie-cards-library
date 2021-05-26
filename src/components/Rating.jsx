938be29c69c6ef08691fabaa7be0b466a9e0dd59
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-26 15:43:22 -0300

    Acho que agora corrigi o lint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,19 @@
 // implement Rating component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Rating extends Component {
   render() {
     const { rating } = this.props;
+    console.log(rating);
     return <p className="rating">{rating}</p>;
   }
 }
-
+Rating.defaultProps = {
+  rating: 0,
+};
 Rating.propTypes = {
-  rating: PropTypes.number.isRequired,
+  rating: PropTypes.number,
 };
 
 export default Rating;
