11aa30b9f891ffd8dc78931b8afc22ad3904e520
Alberto Candido <albertocandido98@gmail.com>
2021-05-26 16:28:53 -0300

    Corrige erros lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,15 @@
 // implement MovieList component here
 import React from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props.movies
     return (
       <div>
-        { this.props.movies.map((movie )=> <MovieCard movie= { movie } key= { movie.title } />) }
+        { movies.map(
+          (movie) => <MovieCard movie={ movie } key={ movie.title } />) }
       </div>
     );
   }
