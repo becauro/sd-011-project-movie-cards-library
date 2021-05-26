de531546f1f3a6a16ba3af2088c7d1d926fe1062
Daniel Leon <daniel.radialista@gmail.com>
2021-05-26 18:12:03 -0300

    requisitos 10 ao 14, prontos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyLine, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
         <img src={ imagePath } alt={ title } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyLine}</p>
+        <div>
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+        </div>
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -20,9 +24,9 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
-    storyLine: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
-    // rating: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
   }).isRequired,
 };
 
