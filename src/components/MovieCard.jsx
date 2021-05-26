304ba34220ecfbe5f77acdeacc68cfa54e0b754f
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-26 14:29:24 -0300

    Adiciona requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,6 +10,7 @@ export class MovieCard extends Component {
         <img src={ imagePath } alt="movie" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
+        <p>{storyline}</p>
       </div>
     );
   }
