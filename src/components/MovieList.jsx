4fce5e7583480910a3dae82f014e4cba3b07a97c
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-26 16:31:04 -0300

    task 2 reset

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,9 @@
 // implement MovieList component here
 
-import React, { Component } from 'react';
-import MovieCard from 'MovieCard';
+import React from 'react';
 
 class MovieList extends React.Component {
   render() {
-    const [{ title, subtitle, storyline, rating, imagePath }] = this.props.movies;
-    return (
-      <section>
-        { this.props.movies
-          .map((movie) => <MovieCard key={movie.title} movies={movie} />) }
-      </section>
-    );
+
   }
 }
-
-export default MovieList;
