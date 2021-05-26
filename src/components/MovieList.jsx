11c6e09b2ef19083a910b325397f16d87e691160
IsaacMagno <isaacmagno98@gmail.com>
2021-05-26 16:08:37 -0300

    izi demais

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const {movies} = this.props;
+
+    return (
+      <div>
+        {movies.map((movie, index) => (
+          <MovieCard movie={movie} key={'Movie Title ' + (index + 1)}/>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default MovieList;
