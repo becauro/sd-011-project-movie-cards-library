6bf634b244fd5f2ac15ffc09c770cfba159661e8
MIkaela Braga <bragamikaela@gmail.com>
2021-05-26 17:43:37 -0300

    tentando arrumar lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,14 +3,14 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    const { title, subtitle, storyline, rating, imagePath, rating } = this.props;
     return (
       <div>
         <img src= { imagePath } alt="filmes" />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <Rating />
+        <Rating rating= { rating }/>
       </div>
     )
   }
