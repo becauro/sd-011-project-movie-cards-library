849753de3f47e25277e3000776e597789f8199aa
Gisele Costa <jikinha@gmail.com>
2021-05-26 15:11:36 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+// <MovieList /> deve receber uma prop movies, que é um array de objetos com informações de um filme.
+
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
+
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default MovieList;
