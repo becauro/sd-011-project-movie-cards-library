c28931933fb74c98d642c45551ece5bdca204b62
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-05-26 20:36:49 -0300

    Requisitos at[e o 16 realizados

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return null;
+    const { rating } = this.props;
+    return (
+      <div className="rating">
+        <span>{rating}</span>
+      </div>
+    );
   }
 }
+
+Rating.propTypes = {
+  rating: propTypes.string.isRequired,
+};
 
 export default Rating;
