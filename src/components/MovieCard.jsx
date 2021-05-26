ab63b1ddc1e070e663292d344f450f8e7ba21f59
Gisele Costa <jikinha@gmail.com>
2021-05-26 15:25:45 -0300

    e agora, lint?

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,2 @@
-// implement MovieCard component here
+// Crie um componente que represente um cartão de filme. <MovieCard /> deve receber uma prop movie. Essa prop será um objeto, contendo as propriedades, title, subtitle, storyline, imagePath e rating.
+
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,14 @@
-// <MovieList /> deve receber uma prop movies, que é um array de objetos com informações de um filme.
-
 import React, { Component } from 'react';
 import MovieCard from 'MovieCard';
 // import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
+    const [{ title, subtitle, storyline, rating, imagePath }] = this.props.movies;
 
     return (
       <div>
-        {this.props.movies.map(movie => <MovieCard key={movie.title} movies={movie} />)}
+        {this.props.movies.map((movie) => <MovieCard key={ movie.title } movies={ movie } />)}
       </div>
     );
   }
