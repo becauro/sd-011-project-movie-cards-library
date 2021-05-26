e54049ba1ebd76eab1b5861da3e326e777e73335
Michelle <mls.srbek@gmail.com>
2021-05-26 16:55:37 -0300

    req 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { render } from 'enzyme';
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return(
-            <h1>Movie Cards Library</h1>
-        )
-    } 
+  render() {
+    return (
+      <h1>Movie Cards Library</h1>
+    )
+  };
 }
 
 export default Header;
