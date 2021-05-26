a4207ba7ff20e5f166581baa056f9072b91b346e
JoaoCabrera <joaocabrerati@gmail.com>
2021-05-26 15:20:42 -0300

    Adiciona requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 class Header extends React.Component {
-  render() {
+  render () {
     return (
-      <header />
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
