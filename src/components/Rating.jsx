55aca936aa584c161be7bc309ec45630fc86bdc2
Matheus Gaspar <msouzagaspar@gmail.com>
2021-05-26 16:35:23 -0300

    adiciona classes css

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,9 +5,10 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div>
-        <p>Rating</p>
-        <p className="rating">{rating}</p>
+      <div className="movie-card-rating">
+        <p className="rating">
+          {rating}
+        </p>
       </div>
     );
   }
