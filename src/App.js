582ef0dcc1d6ec82d59b67f6a6fe27976b11b98f
heyynat <natalifplima@gmail.com>
2021-05-26 13:17:21 -0400

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+
 
 class App extends React.Component {
   render() {
@@ -8,6 +10,7 @@ class App extends React.Component {
       <div className="App">
 
       <Header />
+      <MovieList />
 
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
       
