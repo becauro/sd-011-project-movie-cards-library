cdc4976adfdc9b8a0dbf116360909a535f0db246
Víctor Faria <vitilevi@hotmail.com>
2021-05-26 15:05:15 -0300

    Corrigido lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
-  render() { 
-    return ( 
+  render() {
+    return (
       <header>
         <h1>
           Movie Cards Library
         </h1>
       </header>
-     );
+    );
   }
 }
  
-export default Header
+export default Header;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
-  render() { 
-    const movie = this.props.movie
+  render() {
+    const movie = this.props.movie;
     return ( 
       <div>
-        
+
       </div>
-     );
+    );
   }
 }
- 
+
+MovieCard.PropTypes = {
+  movie: string.required,
+}
+
 export default MovieCard;
