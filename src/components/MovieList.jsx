6f6bf7756ecffaa79b53a324fee6f9269aa8a231
LeoFuna <leo.funa@gmail.com>
2021-05-26 14:41:38 -0300

    Mais ajustes linter

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,20 +1,20 @@
 // implement MovieList component here
-import React from 'react'
-import MovieCard from './MovieCard'
-import propTypes from 'prop-types'
+import React from 'react';
+import propTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     return (
       <div>
-        { this.props.movies.map((movie) => <MovieCard movie={movie} key={movie.title}/>) }
+        { this.props.movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />) }
       </div>
-    )
+    );
   }
 }
 
 MovieList.propTypes = {
-  movies: propTypes.array
+  movies: propTypes.array,
 };
 
-export default MovieList
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,17 +1,17 @@
 // implement Rating component here
-import React from 'react'
-import propTypes from 'prop-types'
+import React from 'react';
+import propTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return(
-      <p className='rating'>{this.props.rating}</p>
+    return (
+      <p className="rating">{this.props.rating}</p>
     )
   }
 }
 
 Rating.propTypes = {
-  rating: propTypes.number
+  rating: propTypes.number,
 };
 
-export default Rating
+export default Rating;
