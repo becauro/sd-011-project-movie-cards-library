cd3c0e19adccc1675c4f8a163ef204c17c23d4cd
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-05-26 16:14:12 -0300

    requisitos 3 ~ 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+
+    return(
+      <div>
+        Filme: {title}, {subtitle}
+        Sinopse: {storyline}
+        Imagem: {imagePath}
+        Classificação: {rating}
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard'
+
+class MovieList extends React.Component {
+  render() {
+
+    const { movies } = this.props;
+
+    return(
+      <div>
+        {movies.map((movie, index) => <MovieCard movie={movie} key={index} />)}
+      </div>
+    )
+  }
+}
+
+export default MovieList;
