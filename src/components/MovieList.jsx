f150716df602b9d6ef1fff3fd0166d675336355c
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-05-26 16:06:08 -0300

    implementa componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import Rating from './Rating';
+
+class MovieList extends React.Component {
+  render() {
+    const movies = this.props.movies;
+    return (
+        movies.map((item) => {
+          return <div className = 'movie-container'>
+            <MovieCard movie = {item} />
+            <Rating score = {item.rating}/>
+          </div>
+        })
+    );
+  }
+}
+
+export default MovieList;
