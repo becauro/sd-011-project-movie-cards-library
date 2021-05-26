d4a4ffddc3d6ebd91f455861db4b78de91b39aff
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-26 14:19:42 -0300

    Adiciona requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import React, { Component } from 'react';
 export class Header extends Component {
   render() {
     return (
-
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
