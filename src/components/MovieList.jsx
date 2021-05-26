582245da9f16490ad4a623d30e9252bae06d8750
josanjohnata <josanjohnata@gmail.com>
2021-05-26 17:04:30 -0300

    corrigindo o Lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import Data from '../data'
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard'
 
 class MovieList extends React.Component {
   render() { 
+    const [movies] = this.props;
     return (
-      <MovieList />
+      <div>
+        { movies.map((movie, index) => <MovieCard key={ index } movie={ movie } />) }
+      </div>
     );
   }
+}
+// Ajuda do Luiz na sala "A"
+
+MovieList.PropTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired
 }
 
 export default MovieList;
