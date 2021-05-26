2509a2077dd378b5a8f4c1ea79cd6ac130e35196
Felipe Buso <felipe_buso@hotmail.com>
2021-05-26 17:38:39 -0300

    correçao lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
+    const movies = this.props.movies;
     return (
       movies.map((movie) => <MovieCard movie={ movie } key={ movie.title }/>)
     );
