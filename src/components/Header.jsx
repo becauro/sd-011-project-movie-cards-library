6bbcf6d26ab337d399bfe3b4b3fcaaf796f27327
guinhoanjos <iago.anjos@hotmail.com>
2021-05-26 16:31:10 -0300

    botando h1 dentro do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <h1 className="header"> Movie Cards Library </h1>;
+    return (
+        <header>
+            <h1 className="header">Movie Cards Library</h1>
+        </header>
+    );
   }
 }
 
