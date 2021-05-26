16ea3f164115178fabe05184cb899a03c972502a
Jessica Arruda <sicarruda@gmail.com>
2021-05-26 19:14:52 -0300

    exercicios do 11 ao 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { imagePath, title, subtitle, storyline } = this.props;
+    const { imagePath, title, subtitle, storyline, rating } = this.props;
     return (
-      <div>
-        <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -20,6 +22,7 @@ MovieCard.propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.string,
   storyline: PropTypes.string,
+  rating: PropTypes.number,
 };
 
 MovieCard.defaultProps = {
@@ -27,6 +30,7 @@ MovieCard.defaultProps = {
   title: 'Movie Title 1',
   subtitle: 'Movie Subtitle 1',
   storyline: 'Movie Storyline 1',
+  rating: 4.5,
 };
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
+      <div className="movie-list">
         {movies.map((movie) => (
           <MovieCard
             key={ movie.title }
@@ -14,6 +14,7 @@ class MovieList extends React.Component {
             title={ movie.title }
             subtitle={ movie.subtitle }
             storyline={ movie.storyline }
+            rating={ movie.rating }
           />
         ))}
       </div>
