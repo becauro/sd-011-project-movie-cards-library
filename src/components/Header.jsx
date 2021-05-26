27efeae56832fc86687c27ccb0aa26ceb171b86a
marcoswarmling <marcoswarmling15@gmail.com>
2021-05-26 15:05:53 -0300

    requisitos 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-'iniciando o projeto'
+import React, { Component } from 'react'
+
+
+export default class Header extends Component {
+    render() {
+        return (
+          <header>
+              <h1>Movie Cards Library</h1>
+          </header>
+        )
+    }
+}
+
