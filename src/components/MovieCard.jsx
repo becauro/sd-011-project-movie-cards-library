f32d93a9fd53dbbe85dda647e2f25c0587fb61df
Rodolfo <rodolforrcont@gmail.com>
2021-05-26 15:48:08 -0300

    Lint 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,14 +4,14 @@ import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
-        <p className="movie-card-storyline">{ storyline }</p> 
-        <Rating rating={ rating }/> 
+        <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
-    const { rating } = this.props.rating;
+    const { rating } = this.props;
     return (
-    <div className="movie-card-rating">
-      <p><span className="rating">{ rating }</span></p>
-    </div>
+      <div className="movie-card-rating">
+        <p><span className="rating">{ rating }</span></p>
+      </div>
     );
   }
 }
