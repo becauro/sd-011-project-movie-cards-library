0c449cf517582bfbe614ecb09633849fd1053670
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-26 15:57:08 -0300

    adiciona requisito 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,14 +5,14 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <section>
         <img src={ imagePath } alt={ `${title} - ${subtitle} movie cover` } />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <Rating />
+        <Rating rating={ rating } />
       </section>
     );
   }
