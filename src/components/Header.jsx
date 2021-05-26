271454b8d4801a9b0edfcd4b02ff07c127a1d029
marcoswarmling <marcoswarmling15@gmail.com>
2021-05-26 16:19:33 -0300

    erro es lint

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 
 export default class Header extends Component {
-    render() {
-        return (
-          <header>
-              <h1>Movie Cards Library</h1>
+render() {
+    return (
+        <header>
+            <h1>Movie Cards Library</h1>
           </header>
         )
     }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
-// implement MovieCard component here
+import React, { Component } from 'react'
+
+export default class MovieCard extends Component {
+    render() {
+        return (
+            <div>
+                <title></title>
+                <subtitle></subtitle>
+                <storyline></storyline>
+                <imagePath></imagePath>
+                <rating></rating>
+
+            </div>
+        )
+    }
+}
+
