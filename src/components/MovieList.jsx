28bedcc735f0ef0999e1cbefbd67f15fe6003747
Matheus Gaspar <msouzagaspar@gmail.com>
2021-05-26 15:14:30 -0300

    requisitos 4, 5 e 6: cria e renderiza componentes MovieCard

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
-        lista
+        {movies.map((movie) => (
+          <MovieCard movie={ movie } key={ movie.title } />
+        ))}
       </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf({
+    title: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieList;
