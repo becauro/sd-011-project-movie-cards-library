23f28e5b72b4da7400ea182eced5d83b41e5ce41
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-05-26 17:11:02 -0300

    adiciona classes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,20 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <section>
-        <img src={ imagePath } alt={ `${title} - ${subtitle} movie cover` } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={ rating } />
+      <section className="movie-card">
+        <img
+          className="movie-card-image"
+          src={ imagePath }
+          alt={ `${title} - ${subtitle} movie cover` }
+        />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
+        </div>
       </section>
     );
   }
