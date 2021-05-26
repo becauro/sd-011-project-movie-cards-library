fc4f5c7f1000dfec4cc6e1a5a516d7eb4ae1dc1f
JoaoCabrera <joaocabrerati@gmail.com>
2021-05-26 16:05:58 -0300

    Adiciona requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 class MovieList extends React.Component {
   render() {
     return (
-      <div className='movie'>
-      </div>
+      <div className="movie" />
     );
   }
 }
