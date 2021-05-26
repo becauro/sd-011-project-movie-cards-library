7de27cff5bc07d93110ad928ab9957b34d6c8500
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-05-26 18:08:29 -0300

    Exercicio finalizado, a espera de erros do lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
 
     return(
       <div>
-        Filme: {title}, {subtitle}
-        Sinopse: {storyline}
-        Imagem: {imagePath}
-        Classificação: {rating}
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <img src = { imagePath } alt="Folder Film" />
+        <Rating rating = { rating } />
       </div>
     );
   }
