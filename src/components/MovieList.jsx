eb29557c96c03e23f1c40bb110e22e4685cc0a1d
Djaniza Vasques <djanizav@gmail.com>
2021-05-26 17:00:26 -0300

    Corrige eslint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 class MovieList extends React.Component {
-    render() {
-        return(
-            <div></div>
-        )
-    }
+  render() {
+    return (
+      <div>
+    );
+  }
 }
 
 export default MovieList;
