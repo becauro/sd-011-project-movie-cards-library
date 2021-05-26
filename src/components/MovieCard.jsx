82b75124dcba5f2ccb096a7b3fcb17aa58986f97
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-26 15:03:08 -0300

    [Projeto] add Header refaturado 1

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,22 +1,22 @@
 // implement MovieCard component here
-import React, { Component } from 'react'
-import Rating from './Rating';
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath} = this.props.movie;
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div className='movie-card ' >
-        <section className='movie-card-body'>
-          <h4 className='movie-card-title'>{title}</h4>
-          <img className='movie-card-image' src={imagePath}/>
-          <h5 className='movie-card-subtitle'>{subtitle}</h5>
-          <p className='movie-card-storyline'>{storyline}</p>
-          <Rating rating={rating}/>
+      <div className="movie-card ">
+        <section className="movie-card-body">
+          <h4 className="movie-card-title">{ title}</h4>
+          <img className="movie-card-image" src={ imagePath } />
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating rating={ rating } />
         </section>
       </div>
-    )
+    );
   }
 }
 
@@ -28,5 +28,4 @@ MovieCard.propTypes = {
   storyline: PropTypes.string,
   rating: PropTypes.number,
   imagePath: PropTypes.string,
-}
-
+};
