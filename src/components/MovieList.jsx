d35cc90a39bb6b0ffee2d52802c1437f0aa025ce
Ana Jacomassi <anajacomassip@gmail.com>
2021-05-26 17:59:53 -0300

    renderiza Header em App.js

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((item))}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.shape({
+    map: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieList;
