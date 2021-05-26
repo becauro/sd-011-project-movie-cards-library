982d340f1e12cb6deb34b400987da72d7744adff
guinhoanjos <iago.anjos@hotmail.com>
2021-05-26 18:54:41 -0300

    lint 18 54

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
+    const { movies } = this.props;
     return (
       <div>
         {movies.map((value) => <MovieCard movie={ value } key={ value.title } />)}
