2e05be357f407c1024c7af184c0d81edf9ff654c
Leandro de Freitas Piasseta <leandropiassetta@gmail.com>
2021-05-26 15:46:19 -0300

    correcting ESlint| App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Headers />;
+      <Headers />
     </div>
   );
 }
-
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,8 @@ import React from 'react';
 class Headers extends React.Component {
   render() {
     return (
-      <header>
-
-      </header>
-    )
+      <header></header>
+    );
   }
 }
 
