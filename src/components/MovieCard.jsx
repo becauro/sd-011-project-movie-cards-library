da61c08607b946a449bbfe41d695da8b8f1f68e3
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-26 17:57:09 -0300

    MovieCard movie object implemented

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,12 +5,18 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
+    const { movie: {
+      title,
+      subtitle,
+      storyline,
+      rating,
+      imagePath,
+    } } = this.props;
 
     return (
       <section>
         <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>^
+        <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
         <Rating rating={ rating } />
         <img src={ imagePath } alt="movie's art" />
