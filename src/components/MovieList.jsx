3ad97ea24eaca8442fcda02f26b57554a78c7b83
Julio Filizzola <juliofilizzola@hotmail.com>
2021-05-26 15:11:23 -0300

    [Projeto] add movie list

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
 // implement MovieList component here
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list ">
+        {movies.map((item) => (
+          <MovieCard movie={ item } key={ item.title } />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
+
+MovieList.propTypes = {
+  movie: PropTypes.string,
+};
