d7163927b1ff475a2c2da5fd54b9881dc6303583
Ghislaine <glenox79@gmail.com>
2021-05-26 15:29:44 -0300

    Correcao lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import MovieCard from './MovieCard'
+import React from 'react';
+import MovieCard from './MovieCard';
 import movies from '../data';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
 
-    return(
+    return (
       <div>
         {movies.map((card, index) => (<MovieCard movie={card} key={index} />))}
-      </div>   
+      </div>
     );
   }
 }
