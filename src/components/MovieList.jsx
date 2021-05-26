9eb7a0dd20255d219468ab6a20b2055ed5966f73
Luan Xavier <luantenorio3@gmail.com>
2021-05-26 17:12:58 -0300

    Requisitos 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,30 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div />
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } />)}
+      </div>
     );
   }
 }
+
+// Aqui eu consultei este link https://stackoverflow.com/questions/32325912/react-proptype-array-with-shape
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
