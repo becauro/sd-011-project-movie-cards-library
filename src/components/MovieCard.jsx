794c26a5d7cf9ee2563a0b9ad3b2f9ab6c4fed10
josanjohnata <josanjohnata@gmail.com>
2021-05-26 18:24:36 -0300

    Adiciona requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { imagePath, title, subtitle } = movie;
+    const { imagePath, title, subtitle, storyline } = movie;
     return (
       <div>
         <img src={ imagePath } alt="title of the movie" />
         <div>
           <h4>{title}</h4>
           <h5>{subtitle}</h5>
+          <p>{storyline}</p>
         </div>
       </div>
     );
