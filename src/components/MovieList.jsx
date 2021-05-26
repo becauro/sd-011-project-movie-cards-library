c87e087a501dbdc8763af79e8e06bf5c019f3b9e
Felipe Buso <felipe_buso@hotmail.com>
2021-05-26 15:02:52 -0300

    correçao lint Item 1 e 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+// import React from 'react';
+// import PropTypes from 'prop-types';
+
+// class MovieList extends React.Component {
+//   render() {
+
+//   }
+
+// }
+
+// export default MovieList;
