24c773b86142c8fd49825daffe76cd1356ea2d64
Felipe Buso <felipe_buso@hotmail.com>
2021-05-26 17:01:24 -0300

    item 3 e 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    // const movies = this.props.movies;
-    // return (
-    //   movies.map((movie) =>  <MovieList movies = { movies } /> )
-    // )
-  };
+    const movies = this.props.movies;
+    return (
+      movies.map((movie) =>  <MovieCard movie={ movie } /> )
+    );
+  }
 }
 
 export default MovieList;
