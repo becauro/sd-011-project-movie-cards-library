8084674269f6496192571793039b058bd904ae4f
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-26 18:38:16 -0300

    References update & semantic html

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import Header from './components/Header';
 import MovieList from './components/MovieList';
 import movies from './data';
 
+// References:
+// What is Props? http://lucasmaiaesilva.com.br/guia-de-bolso-react/chapter/props.html
+// Typechecking with PropTypes: https://reactjs.org/docs/typechecking-with-proptypes.html
+// Typechecking by Steven Paulino: https://medium.com/@stevenpaulino1/typechecking-with-react-proptypes-6c8f46d78b8a
+
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <Header />
       <MovieList movies={ movies } />
-    </div>
+    </main>
   );
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,16 +7,12 @@ class MovieList extends React.Component {
     const { movies } = this.props;
 
     return (
-      <div>
+      <section>
         { movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />) }
-      </div>
+      </section>
     );
   }
 }
-
-// References:
-// Typechecking: https://reactjs.org/docs/typechecking-with-proptypes.html
-// What is Props? http://lucasmaiaesilva.com.br/guia-de-bolso-react/chapter/props.html
 
 MovieList.propTypes = {
   movies: propTypes.instanceOf(Array).isRequired,
