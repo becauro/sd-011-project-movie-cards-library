5bb04174da68c97d741735124f39c5813e105f10
guinhoanjos <iago.anjos@hotmail.com>
2021-05-26 18:28:53 -0300

    lint 1 rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,8 +3,12 @@ import React from 'react';
 class Rating extends React.Component {
   render() {
     const rate = this.props.rating;
-    return <div className="rating">{ rate }</div>
+    return <div className="rating">{ rate }</div>;
   }
 }
 
 export default Rating;
+
+Rating.protoType = {
+  rating: PropTypes.isRequired,
+};
