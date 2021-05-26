13c1a7c73b00b2b0f841e66a9fa92557846312ef
israeltonholo <israeltonholo@hotmail.com>
2021-05-26 16:04:32 -0300

    adiciona requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,9 @@
-// implement MovieList component here
+import React, { Component } from 'react'
+
+export default class MovieList extends Component {
+  render() {
+    return this.props.movies;
+  }
+}
+
+export default MovieList;
