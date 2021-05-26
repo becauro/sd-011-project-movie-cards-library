0294dbbf43b3fc9e128a1a1b6ff45f20d029f22b
Anajulia Brito <juliehindius@gmail.com>
2021-05-26 17:13:28 -0300

    added a Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <Header tag="h1"> Movie Cards Library </Header>;
+    return (
+      <Header>
+        <h1>Movie Cards Library </h1>
+      </Header>);
   }
 }
 
