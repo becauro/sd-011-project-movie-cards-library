1bc4f97cc74d0b2a17d9b396524ecc208fc28c7d
Víctor Faria <vitilevi@hotmail.com>
2021-05-26 15:01:27 -0300

    Feito requisitos 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+
+class MovieCard extends Component {
+  render() { 
+    const movie = this.props.movie
+    return ( 
+      <div>
+        
+      </div>
+     );
+  }
+}
+ 
+export default MovieCard;
