a03abbf4ab639ffa60df7f9bc90963b0863c6d81
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-05-26 15:22:02 -0300

    requisitos 1 e 3 finalizados

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+
+export default Header
