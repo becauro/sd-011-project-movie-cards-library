b5e37c0196808fdaf47a46d797eb73f44ffa8992
Amandha W. Barok <amandha.barok@gmail.com>
2021-05-26 12:37:10 -0700

    Requirements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-
+import MovieList from './components/MovieList';
+import MoviesData from './data.js'; 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={MoviesData}/>
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
     </div>
   );
