16c943ef9e21a3ee82d46b2f06054bf7be491c85
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-26 15:14:06 -0300

    task 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,15 @@
 // implement Header component here
-// initial commit
+// References:
+// importing Component: https://create-react-app.dev/docs/importing-a-component/
+
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+    <header><h1>Movie Cards Library</h1></header>
+    );
+  }
+}
+
+export default Header;
