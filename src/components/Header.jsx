0e4f7a237d8a4a2456d953bcdbd822bb326f29e1
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-26 15:26:47 -0300

    task 2 and fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-    <header><h1>Movie Cards Library</h1></header>
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.moviesProp;
+    return (
+      <section>
+        {this.props.movies.forEach(movie => <MovieCart key={movie.title} moviesProp={movie} /> )}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
