2b8cc1f605207e5cb8aa0bb8acf3d17c5ee43473
uriel <urielbochi@gmail.com>
2021-05-26 16:24:33 -0300

    Tentativa de correção Lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import MovieList from './MovieList'
 
 class MovieCard extends React.Component {
-  render() {
-      const { title, subtitle, storyline, rating, imagePath} = this.props
-  return (
-      <div>
-      <img src={this.props.imagePath}></img>
-      <h4> {this.props.title}</h4>
-      </div>
-  )
-  }
+    render() {
+        const { title, subtitle, storyline, rating, imagePath } = this.props
+        return (
+            <div>
+                <img src={this.props.imagePath}></img>
+                <h4> {this.props.title}</h4>
+            </div>
+        )
+    }
 }
 
 export default MovieCard;
