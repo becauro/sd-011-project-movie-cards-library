5af23828fbf64ad533bd11dca411903fd9e83225
Rodolfo <rodolforrcont@gmail.com>
2021-05-26 15:14:39 -0300

    Movie Cards Library

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <div className='movie-list'>
+        { movies.map((value) => (
+          <MovieCard className='movie-list' movie={value} key={value.title} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default MovieList;
+
+
+MovieList.propTypes = {
+  movie: PropTypes.object
+}
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
-// implement Rating component here
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    const rating = this.props.rating;
+      return (
+          <div className='movie-card-rating'>
+            <p><span className='rating'>{rating}</span></p>
+          </div>
+      )
+  }
+}
+
+export default Rating;
+
+Rating.propTypes = {
+  rating: PropTypes.number
+}
