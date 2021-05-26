cce3f2a456135d61ec5a75b1174f26407c848a0f
Gisele Costa <jikinha@gmail.com>
2021-05-26 15:17:48 -0300

    requisito 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,7 @@
 // <MovieList /> deve receber uma prop movies, que é um array de objetos com informações de um filme.
 
 import React, { Component } from 'react';
+import MovieCard from 'MovieCard';
 // import PropTypes from 'prop-types';
 
 class MovieList extends Component {
@@ -8,9 +9,9 @@ class MovieList extends Component {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
 
     return (
-      <header>
-        <h1>Movie Cards Library</h1>
-      </header>
+      <div>
+        {this.props.movies.map(movie => <MovieCard key={movie.title} movies={movie} />)}
+      </div>
     );
   }
 }
