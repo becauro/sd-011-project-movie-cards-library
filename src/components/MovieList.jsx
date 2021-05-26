ae732cb3757c06d396b84249586b8efea0cb8e52
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-05-26 18:05:50 -0300

    Adiciona comentario ao requisito 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -13,6 +13,13 @@ class MovieList extends React.Component {
   }
 }
 
+/** A princípio, para resolver este requisito, recebi orientações do estudante
+ * Tales Coelho, estudante da Turma 11 e que já havia terminado o projeto, de
+ * como fazer a verificação de tipos.
+ * Além disso, consultei esta Thread no Slack para entender melhor como utilizar.
+ * Source: https://trybecourse.slack.com/archives/C01PLFW7347/p1622059737335700
+ */
+
 MovieList.propTypes = {
   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
