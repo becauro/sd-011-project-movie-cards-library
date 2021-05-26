559747e455f2d24ad32ca7cd798096b635eafeeb
Hamander <1hamander@gmail.com>
2021-05-26 17:28:04 -0300

    CSS fixes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,12 +7,12 @@ class MovieCard extends Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <div>
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <img src={ imagePath } alt="imagePath" />
-        <Rating rating={ rating } />
+      <div className="movie-card">
+        <img src={ imagePath } alt="imagePath" className="movie-card-image" />
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <h4 className="movie-card-title">{title}</h4>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating className="movie-card-rating"rating={ rating } />
       </div>
     );
   }
