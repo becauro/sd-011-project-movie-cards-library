100404d7fbf8aa98d6a5f9e754eb96f2c17e1d69
Gisele Costa <jikinha@gmail.com>
2021-05-26 16:33:57 -0300

    agora vai, lint!

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
 
     return (
       <div>
