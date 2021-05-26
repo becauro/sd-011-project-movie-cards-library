bfcc76a436b9e08d24a42e9984a985303578345a
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-05-26 20:10:15 -0300

    Corrige erros do ESLint para o arquivo MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -7,15 +8,24 @@ class MovieCard extends React.Component {
 
     return (
       <div>
-        <img src={ imagePath }/>
+        <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyLine }</p>
         <p>{ rating }</p>
-        <hr></hr>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyLine: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
