00458d5e875572cc17127ab3a2b41fceca03b6bc
guinhoanjos <iago.anjos@hotmail.com>
2021-05-26 18:50:41 -0300

    lint 18 50

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
@@ -12,8 +13,16 @@ class MovieList extends React.Component {
   }
 }
 
-// MovieList.protoType = {
-//   movie: PropTypes.string,
-// };
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
