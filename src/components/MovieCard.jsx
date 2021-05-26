e6fd0dbbf01b3c2ecff7a902796bc6ddb8670147
camiloLelis <lafirmafuerte@gmail.com>
2021-05-26 19:13:18 -0300

    requisito3-tentativa-2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const {movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
     return (
-      <section className='movie-card'>
+      <section className="movie-card">
         <div className="movie-card-body">
           <h1 className="movie-card-title">{title}</h1>
           <h2 className="movie-card-subtitle">{subtitle}</h2>
           <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <img src={imagePath} alt= {`capa do filme ${title}`} className="movie-card-image"/>
+        <img src= { imagePath}  alt= {title} className= "movie-card-image" />
       </section>
     );
   }
