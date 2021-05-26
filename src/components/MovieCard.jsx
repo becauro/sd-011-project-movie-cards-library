e8bea0b198a3268bf537ba680ed987c9d22138ba
marlon307 <marlon-ramosb@hotmail.com>
2021-05-26 15:44:17 -0300

    part 3-4-5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div>
+        <img src={imagePath} alt="" />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import data from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return data.map((movie, index) => <MovieCard movie={movie} key={index} />);
+  }
+}
+
+export default MovieList;
