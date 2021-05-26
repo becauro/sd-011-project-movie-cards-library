79b5ba43a3194892c7aa9fb8228a42ad5a9ec8fb
Alexandre <naitwa.alexandre@gmail.com>
2021-05-26 16:58:49 -0300

    done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Rating from './Rating'
 
 class MovieCard extends Component {
   render() {
@@ -12,6 +13,7 @@ class MovieCard extends Component {
       <h4>{title}</h4>
       <h5>{subtitle}</h5>
       <p>{storyline}</p>
+      <Rating rating={rating} />
     </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,20 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  render() {
+    //console.log(this.props.rating)
+
+    return (
+      <div className="rating">
+        {this.props.rating}
+      </div>
+    );
+  }
+}
+
+export default Rating;
+
+Rating.propTypes = {
+  rating: PropTypes.number
+}
