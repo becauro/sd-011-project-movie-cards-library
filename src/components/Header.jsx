b11dee02116561cd343af9d3f2cf4a5bc922d8ef
Arthur Hermann <arthurhermannc@gmail.com>
2021-05-26 19:24:45 -0300

    correcao header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div className="header">
+      <header className="header">
         <h1>Movie Cards Library</h1>
-      </div>);
+      </header>);
   }
 }
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -12,7 +12,6 @@ class Rating extends React.Component {
     );
   }
 }
-
 Rating.propTypes = {
   rating: PropTypes.number.isRequired,
 };
