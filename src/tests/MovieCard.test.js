d7444aeaf04b4bb345fc04f5e74f0ffe89621ee3
Renan <renan.vamo@gmail.com>
2021-05-26 15:18:24 -0300

    Adiciona os ex 1, 2, 15 e 16

--- src/tests/MovieCard.test.js
+++ src/tests/MovieCard.test.js
@@ -29,7 +29,7 @@ const movies = [
 describe('6 - Crie um componente `<MovieCard />`', () => {
   const movie = movies[0];
 
-  it('Renderize o componente `<MovieCard />`', () => {
+  itz('Renderize o componente `<MovieCard />`', () => {
     shallow(<MovieCard movie={ movie } />);
   });
 });
