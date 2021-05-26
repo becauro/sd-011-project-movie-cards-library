6d98227c9c534c69fc847085fdc65d36a2ed38f1
Marco Galindo <marco1408@gmail.com>
2021-05-26 16:11:24 -0300

    rating finalizado

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,8 +3,17 @@ import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
-    return ();
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <div className="rating">{rating}</div>
+      </div>
+    );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
