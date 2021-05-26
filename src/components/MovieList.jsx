5b4a91fc68edf78f82ce560886d4a38660a0a44c
Tulio Sploradori <tuliospl@gmail.com>
2021-05-26 17:22:15 -0300

    teste item 1 e 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+// import React from 'react';
+
+// class MovieList extends React.Component {
+//   render() {
+//     const { movies } = this.props;
+//     return (
+//       <div>
+//         { movies.map(item, index) => <MovieCard movie= { item } key= { index } /> };
+//       </div>
+//     );
+//   }
+// }
+
+// export default MovieList;
