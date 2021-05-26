4c5115f88a1d65f72e704d8840918cb6f8175a03
Júlio Mota <juliomotacampos@gmail.com>
2021-05-26 17:59:50 -0300

    Ajuste no estilo - retirado a img da div movie card body

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,8 +9,9 @@ class MovieCard extends Component {
 
     return (
       <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ `Filme ${title}` } />
+        
         <div className="movie-card-body">
-          <img className="movie-card-image" src={ imagePath } alt={ `Filme ${title}` } />
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
