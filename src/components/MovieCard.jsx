48b72513eb7ad3310e48ddea5ade7f3458db4787
benlopes <rubenssouss@hotmail.com>
2021-05-26 16:15:04 -0300

    Render movies titles, subtitles, storyline and pics within MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { imagePath } } = this.props;
-    // const { imagePath } = this.props.movies;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <div>
         <img src={imagePath} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
       </div>
       )
     }
