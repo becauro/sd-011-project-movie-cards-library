a0caa0bc677cfaa67119860c98ff35333f7a89d6
Alberto Candido <albertocandido98@gmail.com>
2021-05-26 16:35:51 -0300

    Lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,18 @@
 // implement MovieCard component here
 import React from 'react';
-import Rating from './Rating';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    const { movie } = this.props
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <img src= { imagePath } alt= { title } />
+        <img src={ imagePath } alt={ title } />
         <Rating rating={ rating } />
       </div>
     );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,11 +5,12 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props.movies
+    const { movies } = this.props
     return (
       <div>
         { movies.map(
-          (movie) => <MovieCard movie={ movie } key={ movie.title } />) }
+          (movie) => <MovieCard movie={ movie } key={ movie.title } />)
+        }
       </div>
     );
   }
