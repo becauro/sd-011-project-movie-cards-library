fa064b7d3ba2dfbae378733b7d4005ab8d23e4cf
Daniel <danielfdoficial@gmail.com>
2021-05-26 16:08:30 -0300

    refatora dnv PropTypes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,18 @@
 // implement MovieList component here
 import React from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      movies.map((movie) => (<MovieCard key={ movie.title }/>)));
+      movies.map((movie) => (<MovieCard key={ movie.title } />)));
   }
 }
 
 MovieList.propTypes = {
   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
+};
 
 export default MovieList;
