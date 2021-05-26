414752e398724c6223518bf26054f952de36af7f
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-05-26 20:56:27 -0300

    Requisito 15

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
       <div>
-        { rating }
+        <p>Nota:</p>
+        <h3 className="rating">{ rating }</h3>
       </div>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
