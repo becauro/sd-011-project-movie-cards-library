75ec2f8c200e5b82deeac46f358123b0bdda5d3a
WalterCosta <156-201707140059@users.noreply.gitlab.lasse.ufpa.br>
2021-05-26 15:35:36 -0300

    Feito requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,15 @@
 // Iniciando projeto
 
 import React from 'react';
+
 class Header extends React.Component {
   render() {
-    return (
-			<header>
-				<h1>Testando requisito 1</h1>
-			</header>
-		);
-  }
+   return (
+    <header>
+      <h1>Movie Cards Library</h1>
+    </header>
+  );
+}
 }
 
 export default Header;
