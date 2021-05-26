f61b28f0ed9325cffd621422deeed2f2f1a62041
Arthur Hermann <arthurhermannc@gmail.com>
2021-05-26 15:19:42 -0300

    Parte da classe MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const moviesList = this.props.movies;
+    return (
+      <div className="MovieList">
+        {moviesList.map((movie, index) => <MovieCard key={ index } movies={ movie } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
