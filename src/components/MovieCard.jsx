b83e9f773c30438cb6da63ae5bb0db75c49cb128
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-05-26 20:14:45 -0300

    Corrige palavra errada no arquivo MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath, subtitle, storyLine, rating } = movie;
+    const { title, imagePath, subtitle, storyline, rating } = movie;
 
     return (
       <div>
         <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
-        <p>{ storyLine }</p>
+        <p>{ storyline }</p>
         <p>{ rating }</p>
       </div>
     );
@@ -22,7 +22,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
-    storyLine: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
   }).isRequired,
