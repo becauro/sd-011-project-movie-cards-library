87e7d0484b497589318b5356ee30c75c52dcf58a
josanjohnata <josanjohnata@gmail.com>
2021-05-26 20:32:42 -0300

    Adiciona Stylo no projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,14 +7,16 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { imagePath, title, subtitle, storyline, rating } } = this.props;
     return (
-      <main>
-        <img src={ imagePath } alt="title of the movie" />
-        <div>
-          <h4>{title}</h4>
-          <h5>{subtitle}</h5>
-          <p>{storyline}</p>
+      <main className="movie-card">
+        <img src={ imagePath } alt="title of the movie" className="movie-card-image"/>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
         </div>
+        <div className="movie-card-rating">
           <Rating rating={ rating } />
+        </div>
       </main>
     );
   }
