391404248e6ce2049c404d9097e41839b1e9027a
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-05-26 20:39:37 -0300

    Requisito 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
       <div>
+        { rating }
       </div>
     );
   }
