bd6adcd67dadeedec6f6c4cce9a2ca5b876927ab
benlopes <rubenssouss@hotmail.com>
2021-05-26 15:03:18 -0300

    Render MovieCar component within MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,14 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard'
 
 class MovieList extends React.Component {
   render() {
+    // const 
     return (
+
       <section>
+      {this.props.movies.map((movie, index) => <MovieCard movies={movie}/>)}
       </section>
     )
   }
