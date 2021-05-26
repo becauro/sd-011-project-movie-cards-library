1a5ce04ae0d5757ad93f1bfd19f38f0c7029a171
Laura Gusmao <laurargus@gmail.com>
2021-05-26 17:49:35 -0300

    Bomba

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,32 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <main className="movie-list">
+        { movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      imagePath: PropTypes.string,
+    }),
+  ),
+};
+MovieList.defaultProps = {
+  movies: [],
+};
+
+export default MovieList;
