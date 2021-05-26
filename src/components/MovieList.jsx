92b6fbe3742426e89e285958997cdafbcd3cefec
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-26 15:45:41 -0300

    Criando componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.title} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
