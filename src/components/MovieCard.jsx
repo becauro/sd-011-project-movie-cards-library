5c8187f23322f3a53e929bd5275c4df8cc1214ea
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-26 14:33:27 -0300

    Adiciona requisito 13

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React, { Component } from 'react';
+import { Rating } from './Rating';
 
 export class MovieCard extends Component {
   render() {
@@ -11,6 +12,7 @@ export class MovieCard extends Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating />
       </div>
     );
   }
