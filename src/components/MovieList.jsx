59493340b895607195cc116cd35abe76131de085
LeoFuna <leo.funa@gmail.com>
2021-05-26 14:55:20 -0300

    ajustes lint no movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,11 +6,10 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const movies = this.props.movies;
+    console.log(movies)
     return (
       <div>
-        { 
-          movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />) 
-        }
+        { movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />) }
       </div>
     );
   }
