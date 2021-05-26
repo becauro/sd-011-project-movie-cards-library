8f489f23f427d38d5c861dbb413cc565034c5fd2
Jean Esteves <jean.esteves2001@gmail.com>
2021-05-26 11:10:48 -0300

    requisitos 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Header() {
-  return <div></div>;
+  return (
+    <header>
+      <h1>Movie Cards Library</h1>
+    </header>
+  );
 }
 
 export default Header;
