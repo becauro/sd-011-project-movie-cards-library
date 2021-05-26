daee2755fe3ae8c9365991cb182dcb48824cf141
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-05-26 18:19:37 -0300

    Requisitos infinitos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MovieList movies={ movies } />
+
       </div>
     );
   }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,10 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header className='header'>
-       <h1>Movie Cards Library</h1>
+      <header className="movie-card-header">
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
 }
 export default Header;
-
