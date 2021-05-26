fa0e989e10867fc6ad629462e32aaa49b63784cb
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-05-26 16:54:12 -0300

    req3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,18 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      <h1>{movies}</h1>
+      <div>{movies}</div>
     );
   }
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.objects.isRequired,
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieList;
