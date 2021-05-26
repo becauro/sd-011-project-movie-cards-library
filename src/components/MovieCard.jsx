4f5dc08937bd42bb0314c60420451516cc5fedac
Gustavo Lemes <gustavolemes997@gmail.com>
2021-05-26 16:48:34 -0300

    FIRE IN THE HOLE

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 // implement MovieCard component here
 import React, { Component } from 'react';
+import Rating from '../components/Rating';
 
 export default class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
     <div>
       <h4>{title}</h4>
       <h5>{subtitle}</h5>
       <img src = {imagePath} alt = {title}></img>
       <p>{storyline}</p>
+      <Rating rating = {rating} />
     </div>
     );
   }
