a5e9e2b1b8963ab4c222b6a139e759c008265dc0
Gisele Costa <jikinha@gmail.com>
2021-05-26 16:03:37 -0300

    MovieList?

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import MovieCard from 'MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
         {movies.map((movie) =>
@@ -11,5 +13,9 @@ class MovieList extends Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
