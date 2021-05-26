65dc5bf04996af7a67ccf45961498503db89dc17
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-26 18:02:36 -0300

    Adicionando Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() { 
-    const 
-    return ( '' );
+    const {title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return ( 
+      <div>
+        <img src={imagePath} alt={title} />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>storyline</p>
+        <Rating nota={rating} />
+      </div>
+     );
   }
 }
  
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    const rating = this.props.nota;
+    return <p className='rating'>{rating}</p>;
+  }
+}
+
+export default Rating;
