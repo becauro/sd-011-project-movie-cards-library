e3217dfc3583ea2babc57aecd8030267eba54b69
uriel <urielbochi@gmail.com>
2021-05-26 16:21:18 -0300

    Alguns requisitos

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MovieList from './MovieList'
+
+class MovieCard extends React.Component {
+  render() {
+      const { title, subtitle, storyline, rating, imagePath} = this.props
+  return (
+      <div>
+      <img src={this.props.imagePath}></img>
+      <h4> {this.props.title}</h4>
+      </div>
+  )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import MovieCard from './MovieCard'
+
 
 class MovieList extends React.Component {
   render() {
-
+    const { movies } = this.props;
+    return (
+        <div>
+          {movies.map((item) => (
+            <MovieCard movie={ item } key={ item.title } />
+          ))}
+        </div>
+      );
+    }
   }
-}
 
 export default MovieList;
