e92e009d8211ae66238521b23eed84ba1c336067
Hamander <1hamander@gmail.com>
2021-05-26 17:33:00 -0300

    Lint Fixes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,10 +9,10 @@ class MovieCard extends Component {
     return (
       <div className="movie-card">
         <img src={ imagePath } alt="imagePath" className="movie-card-image" />
-        <h5 className="movie-card-subtitle">{subtitle}</h5>
         <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
         <p className="movie-card-storyline">{storyline}</p>
-        <Rating className="movie-card-rating"rating={ rating } />
+        <Rating className="movie-card-rating" rating={ rating } />
       </div>
     );
   }
