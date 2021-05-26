f4a1c259709d42c3a91601f2bd7b789a48816674
MIkaela Braga <bragamikaela@gmail.com>
2021-05-26 16:34:09 -0300

    teste

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render () {
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    return (
+      <div>
+        <img src= { imagePath } alt="filmes">
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
+    )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render () {
+    const { movies } = this.props;
+    return (
+     {movies.map((card, index) => <MovieCard movie= {card} key= {index})}
+    );
+  }
+}
+
+export default MovieList;
