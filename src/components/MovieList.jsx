5f2d7ba428c7c1b526a86db36f2a626dff66b0d3
Tales Coelho <tales-coelho@live.com>
2021-05-26 14:30:43 -0300

    requisito 1 e 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+
+export class MovieList extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    );
+  }
+}
+
+export default MovieList;
