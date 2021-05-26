8e456c5a1b1ceb8bedef512c7b9cae160c43cb46
Gustavo Lemes <gustavolemes997@gmail.com>
2021-05-26 16:39:55 -0300

    Requisito 4,5 e 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,16 @@
 // implement MovieCard component here
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    return (
+    <div>
+      <h4>{title}</h4>
+      <h5>{subtitle}</h5>
+      <img src = {imagePath} alt = {title}></img>
+      <p>{storyline}</p>
+    </div>
+    );
+  }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,13 @@
 // implement MovieList component here
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import MovieCard from '../components/MovieCard';
 
 export default class MovieList extends Component {
   render() {
-    
+    const movies = this.props.movies;
+
+    return movies.map((movieData) => {
+      return <MovieCard key = { movieData.title } movie = { movieData } />
+    })
   }
 }
