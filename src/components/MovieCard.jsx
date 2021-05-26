d8446723aeaec626654b688c5c46cf5694c418b9
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-26 14:26:37 -0300

    Adiciona requisito 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+export class MovieCard extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default MovieCard;
