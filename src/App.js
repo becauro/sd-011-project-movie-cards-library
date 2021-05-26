c04525cccf630a0dab6f3972e03559420d03d6d2
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-05-26 18:28:38 -0300

    Adiciona requisitos 3 ao 10

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import Data from './data';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <body>
+        <ul>
+          <MovieList movies={ Data } />
+        </ul>
+      </body>
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
 // implement MovieCard component here
+import React from 'react';
+import Data from '../data'
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    
+    return (
+      <div>
+        <img src={imagePath} alt="Movie"></img>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
