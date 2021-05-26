97406fd532fac73653aceeca776d00d276db85de
Hugo Mouto <hmoutol@gmail.com>
2021-05-26 18:07:32 -0300

    feat: Critério 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 export default class MovieCard extends Component {
   render() {
+    
+    const { title, img, subtitle, storyline } = this.props;
+
     return (
       <div>
-        oi
+        <img src={img} alt="" />
+        {title}
+        {subtitle}
+        {storyline}  
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 import MovieCard from './MovieCard';
 
+
 export default class MovieList extends Component {
   render() {
     return (
-      <MovieCard />
+      this.props.movies.map((movie) => {
+        return (
+          <div className="movie-card">
+            <MovieCard
+            img = {movie.imagePath}
+            title = {movie.title}
+            subtitle= {movie.subtitle}
+            storyline = {movie.storyline}
+            />
+          </div>
+        )
+      })
     );
   }
 }
