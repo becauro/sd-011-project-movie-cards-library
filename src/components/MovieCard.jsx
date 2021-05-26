00a59224fffa89c8e40b37590268e82e8bd4053e
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-05-26 17:31:42 -0300

    Adiciona varios requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
 // implement MovieCard component here
+
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+    return (
+      <div>
+        <img src = { imagePath } alt = 'movie' />
+        <h4> {title} </h4>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,15 @@
 // implement MovieList component here
 
 import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+    return ( movies.map((elemento) => {
+      <MovieCard key = { elemento.title } movie = { elemento } />
+    }));
   }
 }
 
