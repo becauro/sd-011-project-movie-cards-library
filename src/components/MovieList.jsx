341f1d9a431dc4a4ea185ac7e0a6e88617650d1a
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-05-26 15:42:59 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+// import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    //   const  {title, subtitle, storyline, imagePath} = this.props.movie
+    return (
+      <div>
+        Ola
+        {/* <MovieCard /> */}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
