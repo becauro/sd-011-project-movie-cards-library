f4971d327d2a4409669f210834c8d4f68d687e35
André Jaques Batista <andrejaques.b@gmail.com>
2021-05-26 15:52:23 -0300

    complete requires 01 to 06

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 export default class Header extends Component {
-    
   render() {
     return (
-      <div>
-        <p></p>
-      </div>
+      <header className="header">
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Rating from './Rating';
+
+export default class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+
+    return (
+      <div className="movie-card">
+        
+      </div>
+    )
+  }
+}
