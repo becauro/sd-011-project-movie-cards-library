261553308ab0c19b20a6c46f89b7b6c9a6e99c42
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-05-26 14:29:51 -0300

    Adiciona requisito 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
 // implement Rating component here
+import React, { Component } from 'react';
+
+export class Rating extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default Rating;
