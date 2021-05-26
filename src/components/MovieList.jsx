96792ee0732f7e965856dd14ef49c038babcf474
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-05-26 19:07:31 -0300

    Vários requisitos concluídos

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
-        { movies.map((item, index) => <MovieCard movie={ item } key={ index } />)};
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
+        ))}
       </div>
-      
     );
   }
 }
 
 export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.shape({
+    map: PropTypes.string,
+  }).isRequired,
+};
