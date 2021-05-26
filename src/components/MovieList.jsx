5dcacc1e059259e49cdb6785ce8f3b9346ef5aaa
Maran <maranmorimoto@gmail.com>
2021-05-26 20:16:02 -0300

    fixing lint errors and finishing projects requests

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import MovieCard from '../components/MovieCard';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
-        { movies.map((movie) => <MovieCard movie={ movie } />) }
+        { movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />) }
       </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
