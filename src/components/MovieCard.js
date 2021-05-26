d818e247c15bbad5f9c54747d48e16815bf48364
Lais Simenikim <l.simenikim@gmail.com>
2021-05-26 20:44:05 -0300

    BOMBA add requisitos 3, 4, 5, 6, 7

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,1 +1,28 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        {/* <li>{movie.title}</li>
+        <li>{movie.subtitle}</li>
+        <li>{movie.storyline}</li>
+        <li>{movie.rating}</li> */}
+        <img src={ movie.imagePath } alt="" />
+      </div>);
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
