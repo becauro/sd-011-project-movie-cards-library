8b05bec91a3d6049b69abf65efda5baa9756f776
OldRoge <rogeriomrjr@icloud.com>
2021-05-26 20:31:45 -0300

    Trying send requirement 4 again ):

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,40 @@
 // implement MovieCard component here
-// import React from 'react';
+import propTypes from 'prop-types';
+import React from 'react';
 
-// class MovieCard extends React.Component {
-//   render() {
-//     const { movie } = this.props;
-//     const { title, subtitle, storyline, imagePath } = movie;
-//     return ();
-//   }
-// }
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
+    return (
+      <div>
+        <section>
+          <h4>{ title }</h4>
+          <img src={ imagePath } alt="ImageMovie" />
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+        </section>
+      </div>
+    );
+  }
+}
 
-// export default MovieCard;
+export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: propTypes.string,
+    subtitle: propTypes.string,
+    storyline: propTypes.string,
+    imagePath: propTypes.string,
+  }),
+};
+
+MovieCard.defaultProps = {
+  movie: {
+    title: 'First',
+    subtitle: 'Second',
+    storyline: 'Third',
+    imagePath: 'Fourth',
+  },
+};
