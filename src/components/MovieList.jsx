785b2fff2515ba1f51eba7255dc935a3029ee1de
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-05-26 17:49:23 -0300

    Conserta erros do ESLint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props
+    const { movies } = this.props;
     return (
       <div>
-        {movies.map(((anyMovie) => <MovieCard key={anyMovie.title} />))}
+        { movies.map(((anyMovie) => <MovieCard key={ anyMovie.title } />)) }
       </div>
-    )
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
