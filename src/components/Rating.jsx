8b07514ab43ba8f9e7f0857df90c1f0c9978a2f4
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-26 18:18:12 -0300

    Rating: initial structure

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,15 @@
 // implement Rating component here
+import React from 'react';
+// import propTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    p.className = 'rating';
+    return (
+      <p>{ rating }</p>
+    );
+  }
+}
+
+export default Rating;
