3a7a69013b9fd76049088ac7deb347db1fcc538f
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-05-26 16:07:58 -0300

    Commit inicial

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard'
+   const movies = ({MovieCard});
+
+class MovieList extends Component {
+    render(){
+        return(
+    <h1>{this.props.movies}</h1>
+    )};
+}
+
+export default MovieList;
