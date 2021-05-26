4a0b46dd37e29deca969ab4e415337f65e508a14
KaioHPs <kaiohpsalternativo@gmail.com>
2021-05-26 15:45:21 -0300

    Correçao do Lint

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import MovieList from './components/MovieList';
 
 function App() {
   const moviesArray = [
-    {title: 'Opa', subtitle: 'Ferrou', storyline: 'Ficamos sem', imagePath: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/51F3/production/_106997902_gettyimages-611696954.jpg', rating: 7}
-  ]
+    { title: 'Opa', subtitle: 'Ferrou', storyline: 'Ficamos sem', imagePath: 'https://ichef.bbci.co.uk/news/640/cpsprodpb/51F3/production/_106997902_gettyimages-611696954.jpg', rating: 7 },
+  ];
 
   return (
     <div className="App">
       <Header />
-      <MovieList movies={moviesArray}/>
+      <MovieList movies={ moviesArray } />
     </div>
   );
 }
