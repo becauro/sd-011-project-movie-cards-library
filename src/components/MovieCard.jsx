79eaa5a9b5ab16f1800994dcc5f44ff3c6de9513
uriel <urielbochi@gmail.com>
2021-05-26 18:53:42 -0300

    Erros 6.0

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,10 +6,10 @@ class MovieCard extends React.Component {
     const { movie } = this.props
     return (
       <div>
-        <img src={this.props.movie.imagePath}></img>
-        <h4>{this.props.movie.title}</h4>
-        <h5>{this.props.movie.subtitle}</h5>
-        <p>{this.props.movie.storyline}</p>
+        <img src={movie.imagePath} alt="Image"></img>
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
       </div>
     )
   }
