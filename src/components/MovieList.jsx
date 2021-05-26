4d89db4a25d84aeeba267d54576a1d126607eb68
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-26 15:52:11 -0300

    t2 lint fixes 1.1

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,15 @@
 // implement MovieList component here
 
-import React from 'react';
+import React, { Component } from 'react';
 import MovieCard from 'MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const [{ title, subtitle, storyline, rating, imagePath }] = this.props.moviesProp;
+    const [{ title, subtitle, storyline, rating, imagePath }] = this.props.movies;
     return (
       <section>
-        {this.props.movies.map(movie => <MovieCard key={movie.title} moviesProp={movie} />)}
+        { this.props.movies
+          .map((movie) => <MovieCard key={movie.title} movies={movie} />) }
       </section>
     );
   }
