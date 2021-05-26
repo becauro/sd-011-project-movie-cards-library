34102195b962192ff94b94234f46c55ebe42a184
guinhoanjos <iago.anjos@hotmail.com>
2021-05-26 18:30:14 -0300

    Update Rating.jsx

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    const rate = this.props.rating;
-    return <div className="rating">{ rate }</div>;
+    return <div className="rating">{ this.props.rating }</div>;
   }
 }
 
