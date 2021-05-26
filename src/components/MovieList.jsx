e30800f73503be30f514a4d640f2b62ff5f6c43e
Felipe Buso <felipe_buso@hotmail.com>
2021-05-26 16:12:42 -0300

    Arrumando o package.json

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
-// import React from 'react';
+import React from 'react';
 // import PropTypes from 'prop-types';
+import movies from '../data'
 
-// class MovieList extends React.Component {
-//   render() {
+class MovieList extends React.Component {
+  render() {
+    const moviesList = movies;
+    return (
+      <MovieList movies = {moviesList}/>
+    )
+  };
+}
 
-//   }
-
-// }
-
-// export default MovieList;
+export default MovieList;
