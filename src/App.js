902b257e83feacbfa8c6d3e512d503cf40d7ca38
Diogo Augusto <diogotrescastro@gmail.com>
2021-05-26 15:58:39 -0300

    Implementaçao da MovieList e MovieCard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-// import MovieList from './components/MovieList';
+import MovieList from './components/MovieList';
 // import MovieCard from './components/MovieCard';
 // import Rating from './components/Rating';
-// import data from './data';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ data } />
     </div>
   );
 }
-
 export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,45 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie
+    return (
+      <div>
+        <img src={ imagePath } alt="Movie Cover" />
+        <h3>{ title }</h3>
+        <h4>{ subtitle }</h4>
+        <p>{ storyline }</p>
+        <div>
+          <Rating rating={ rating } />
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.string,
+  }),
+};
+
+// Consulta em para verificar defaultProps https://pt-br.reactjs.org/docs/typechecking-with-proptypes.html
+
+MovieCard.defaultProps = {
+  movie: {
+    title: 'title',
+    subtitle: 'subtitle',
+    storyline: 'storyline',
+    rating: 0,
+    imagePath: 'image',
+  },
+};
+
+export default MovieCard;
