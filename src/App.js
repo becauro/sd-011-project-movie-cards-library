c191ab7d76cdacf9ce105a7f4c1b1d42dfc478ba
Luan Xavier <luantenorio3@gmail.com>
2021-05-26 15:50:17 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieContainer from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieContainer />
     </div>
   );
 }
