f1707f44d0ed39bf4bb3974d7cba0bdace4a179b
Vitor <vitorguima@gmail.com>
2021-05-26 19:26:33 -0300

    tentativa entrega projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 // implement Rating component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
@@ -10,5 +11,13 @@ class Rating extends Component {
     </div>)
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
+};
 
 export default Rating;
