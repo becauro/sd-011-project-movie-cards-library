64447769f9fd664ff112a0ddd37867b576cdcaef
anaclarabck <ackyotoku@gmail.com>
2021-05-26 15:59:01 -0300

    [ADD] incluido header, movielist (ja com cards)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div className='movie-card-body'>
+        <img className='movie-card-header' src={imagePath} /> 
+        <div className='movie-card-title'>{title}</div>
+        {subtitle} {storyline} {rating}
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
