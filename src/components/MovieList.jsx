b400d94f7783dcc9d48556229755f81fb391f7db
PH Assis <pieit@live.com>
2021-05-26 14:27:23 -0300

    Initial Project commit

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+
+export default class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((el) => (
+          <MovieCard key={el.title} movie={el} />
+        ))}
+      </div>
+    );
+  }
+}
+
+// MovieList.propTypes = {
+//   movies: PropTypes.array,
+// };
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,14 @@
 // implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  static propTypes = {
+    rating: PropTypes.number,
+  };
+  render() {
+    return <div className="rating">{this.props.rating}</div>;
+  }
+}
+
+export default Rating;
