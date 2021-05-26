a90ea9b144e3b5718d51cc9286e256ef37efbd6a
VictorS <czpask@gmail.com>
2021-05-26 14:47:07 -0300

    Projeto sem CSS

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <section>{this.props.movies.map((movieObj) => {
+          return <MovieCard key={movieObj.title} movie={movieObj}/>
+        })}</section>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
