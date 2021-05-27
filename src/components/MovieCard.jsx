3daa23d7cc74a15b5cc2e83a0bf735beb0207e10
marsleite <marsleite@gmail.com>
2021-05-27 00:37:12 -0300

    folha de estilo atualizado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,20 @@ class MovieCard extends Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <div>
-        <img src={ imagePath } alt="Movies" />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <div>
+            <img className="movie-card-image" src={ imagePath } alt="Movies" />
+            <h4 className="movie-card-title">{ title }</h4>
+            <h5 className="movie-card-subtitle">{ subtitle }</h5>
+            <div className="story">
+              <p className="movie-card-storyline">{ storyline }</p>
+            </div>
+          </div>
+          <div className="movie-card-rating">
+            <Rating rating={ rating } />
+          </div>
+        </div>
       </div>
     );
   }
