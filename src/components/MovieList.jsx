923526737520f67e2dcb1eb2806634514a735a93
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-26 23:21:09 -0300

    Adicionando CSS e PropTypes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div className='movie-list'>
+      <div className="movie-list">
         {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.title} />
+          <MovieCard movie={ movie } key={ movie.title } />
         ))}
       </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
-    return <p className='rating'>{this.props.rating}</p>;
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <p className="rating">{ rating }</p>
+      </div>
+    );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
