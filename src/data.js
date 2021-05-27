be3e819b6a7a1b73a6fea10ea0ff848aaa8cf18a
Samuel Melo <samuel.silva.melo@hotmail.com>
2021-05-26 21:00:58 -0300

    Correçao erros lint

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ const movies = [
     imagePath: 'images/a-espera-de-um-milagre.jpg',
   },
   {
-    title: 'Interstelar',
+    title: 'Interstellar',
     subtitle: 'Sci-fi/Adventure',
     storyline: 'In Earth\'s future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth\'s population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind\'s new home.',
     rating: 4.9,
