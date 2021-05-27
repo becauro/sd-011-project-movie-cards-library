f4d6cf997cfd8aa3f4a9bdb894e75028fb6655ad
DADA <davimarque2000@gmail.com>
2021-05-27 16:20:36 -0300

    requisito 3, renderiza funçao movie list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import header from './components/Header';
+import MovieList from './components/MovieList';
+import MovieCard from './components/MovieCard';
 
 function App() {
   return (
     <div className="App">
       <header />
+      <MovieList movies={ MovieCard } />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,7 @@
 // implement MovieCard component here
+import React from 'react';
+
+class movieC extends React.Component {
+  render() { return { movies: xblau }; }
+}
+export default movieC;
