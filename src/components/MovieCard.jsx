872495437fdf68e8d55f4005d12c2b31bde392ff
Alberto Candido <albertocandido98@gmail.com>
2021-05-26 21:47:56 -0300

    Corrige erros lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,7 +14,7 @@ class MovieCard extends React.Component {
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
           <img src={ imagePath } alt={ title } className="movie-card-image" />
-       </div>
+        </div>
         <div className="movie-card-rating">
           <Rating rating={ rating } />
         </div>
