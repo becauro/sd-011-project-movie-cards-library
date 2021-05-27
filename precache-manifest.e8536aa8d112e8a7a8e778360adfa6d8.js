4990f43a915fb9f481c0ab73af3535906239d337
Gisele Costa <jikinha@gmail.com>
2021-05-26 22:30:47 -0300

    Updates

---
+++ precache-manifest.e8536aa8d112e8a7a8e778360adfa6d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054e2911f4e66cd9143625e710b50b49",
+    "url": "/project/11_movie_cards_library/index.html"
+  },
+  {
+    "revision": "a988d802baa22f7e32d3",
+    "url": "/project/11_movie_cards_library/static/css/main.e2f7de8e.chunk.css"
+  },
+  {
+    "revision": "0d4f78d6a282aeb41de1",
+    "url": "/project/11_movie_cards_library/static/js/2.9a6a2542.chunk.js"
+  },
+  {
+    "revision": "a988d802baa22f7e32d3",
+    "url": "/project/11_movie_cards_library/static/js/main.6639006a.chunk.js"
+  },
+  {
+    "revision": "859eb0b9750ab1ef5e55",
+    "url": "/project/11_movie_cards_library/static/js/runtime-main.a9f2cf63.js"
+  }
+]);
