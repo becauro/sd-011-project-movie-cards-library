15fe1d4c247bd6be92f24a8a0cbe0839ad1c8997
heyynat <natalifplima@gmail.com>
2021-05-26 21:43:45 -0400

    Requisito 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     return (
+      <div>
       <img src={this.props.movie.imagePath} alt='images/movie_1' />
+      <h4>{this.props.movie.title}</h4>
+      </div>
       )
     }
   }   
