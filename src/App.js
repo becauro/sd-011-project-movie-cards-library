f45d9d313066ab2380f89838b89f7e422e974092
Michelle <mls.srbek@gmail.com>
2021-05-26 22:31:15 -0300

    req 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieList from './components/MovieList';
+import Movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ Movies } />
     </div>
   );
 }
