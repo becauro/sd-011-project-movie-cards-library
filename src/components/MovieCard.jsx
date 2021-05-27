d093b50bd3502816fba93d88a858f67cec7feb32
Vinicius <viniciusfod@gmail.com>
2021-05-27 11:05:50 -0300

    adiciona requisito 11, 12, 13, 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../App.css';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, /* rating */ imagePath } = movie;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
 
     return (
       <div className="movie-card">
@@ -13,6 +14,7 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
