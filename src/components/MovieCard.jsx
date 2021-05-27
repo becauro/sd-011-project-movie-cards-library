ab1a004dde81baf7c3e73056180d03b2414f23b6
Raphael <raphaellfpg@gmail.com>
2021-05-27 12:35:45 -0300

    Sopra salvar

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movies: { title, subtitle, imagePath, storyline, rating }} = this.props;
+    return (
+     <div>
+       <img src = {imagePath} alt={title}/>
+       <h4>{title}</h4>
+       <h5>{subtitle}</h5>
+       <p>{storyline}</p>
+     </div>
+    )
+  }
+}
+
+
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+     <div>
+       { movies.map((item) => {
+        return <MovieCard movies = { item } key = {item.title}/>;
+       })}
+     </div>
+    )
+  }
+}
+
+export default MovieList;
