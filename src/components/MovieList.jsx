646ab16f494b72a56895526507d02955ce2d64db
IgorMendez <igor.emp@hotmail.com>
2021-05-27 20:59:36 -0300

    Movie List

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-
+import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <section></section>
+      <section>
+        { movies.map((e) => <MovieCard movie={ e } key={ e.title } />) }
+      </section>
     )
   }
 }
