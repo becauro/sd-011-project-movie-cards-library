a6126b376d1b2604ee1fe90b21ca130f8757cb18
Dário <dario.junior.rs@gmail.com>
2021-05-27 19:31:19 -0300

    resolve o import dos movies

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import movies from './data';
 import './App.css';
-
-const movies = [
-  {
-    title: 'Final Fantasy Spirits Within',
-    subtitle: 'Movie Subtitle 1',
-    storyline: 'Movie Storyline 1',
-    rating: 4.5,
-    imagePath: 'images/Final_Fantasy_Spirits_Within.jpg',
-  },
-  {
-    title: 'Kingsglave Final Fantasy XV',
-    subtitle: 'Movie Subtitle 2',
-    storyline: 'Movie Storyline 2',
-    rating: 4.5,
-    imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
-  },
-  {
-    title: 'Ghost In The Shell',
-    subtitle: 'Movie Subtitle 3',
-    storyline: 'Movie Storyline 3',
-    rating: 3,
-    imagePath: 'images/Ghost_In_The_Shell_2_0.jpg',
-  },
-];
 
 function App() {
   return (
