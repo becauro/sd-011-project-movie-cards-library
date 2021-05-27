3a2248583abea2bc21220c69ab782228d7737fd2
marsleite <marsleite@gmail.com>
2021-05-26 22:37:45 -0300

    Projet react done #vqv :rocket:

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { movie: {title, subtitle, storyline, rating } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <div>
-        <li>{ title }</li>
-        <li>{ subtitle }</li>
-        <li>{ storyline }</li>
-        <li>{ rating }</li>
+        <img src={ imagePath } alt="Movies" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
