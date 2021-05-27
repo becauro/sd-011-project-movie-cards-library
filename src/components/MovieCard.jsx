14241e4f56f90af7a2cab04017b97dbc483e7247
Luan Xavier <luantenorio3@gmail.com>
2021-05-27 09:27:17 -0300

    Arrumar requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,9 +14,9 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
-          <p className="movie-card-rating">
+          <span className="movie-card-rating">
             <Rating className="rating" rating={ rating } />
-          </p>
+          </span>
         </div>
       </div>
     );
