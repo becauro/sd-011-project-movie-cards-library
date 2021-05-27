d7e0b298b957148f4717e47ec18480e2a88432c5
Michelle <mls.srbek@gmail.com>
2021-05-26 23:57:45 -0300

    req ... destructuring correction

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ import Rating from './Rating';
 class MovieCard extends React.Component {
   render() {
     // eslint-disable-next-line
-    const { movie : { title, subtitle, storyline, rating, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <content>
         <img src={ imagePath } alt="movieImage" />
