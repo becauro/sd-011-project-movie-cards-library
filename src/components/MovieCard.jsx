52b61cefaf0c99637b42487e7c329977b3b036aa
Nalu <aluziapantoja@gmail.com>
2021-05-27 15:33:46 -0300

    Imagens renderizadas

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
 // implement MovieCard component here
+import React, { Component } from 'react'
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    return (
+      <div>
+        <img src={ imagePath }></img>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,16 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div>teste</div>
+      <div>
+        { movies.map((film) => (
+          <MovieCard movie = { film } key = { film.title }/>
+        )) }
+      </div>
     );
   }
 }
