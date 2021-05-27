3eb9d5b1cc87fb8b28437b27948368d394862bbf
Michelle <mls.srbek@gmail.com>
2021-05-26 22:38:23 -0300

    req 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,12 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <div />
+      <MovieCard />
     );
   }
 }
