2d986bf3b376ee2c96ae93820b85837de360175a
Hugo Mouto <hmoutol@gmail.com>
2021-05-26 23:38:13 -0300

    feat: Critérios 4 e 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class MovieCard extends Component {
   render() {
-    
     const { title, img, subtitle, storyline } = this.props;
 
     return (
       <div>
-        <img src={img} alt="" />
+        <img src={ img } alt="" />
         {title}
         {subtitle}
-        {storyline}  
+        {storyline}
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string,
+  storyline: PropTypes.string,
+  subtitle: PropTypes.string,
+};
+
+MovieCard.defaultProps = {
+  img: '',
+  title: '',
+  storyline: '',
+  subtitle: '',
+};
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-
 
 export default class MovieList extends Component {
   render() {
+    const { movies } = this.props;
+
     return (
-      this.props.movies.map((movie) => {
-        return (
-          <div className="movie-card">
-            <MovieCard
-            img = {movie.imagePath}
-            title = {movie.title}
-            subtitle= {movie.subtitle}
-            storyline = {movie.storyline}
-            />
-          </div>
-        )
-      })
+
+      movies.map(({ imagePath, title, subtitle, storyline }) => (
+        <div className="movie-card" key={ title }>
+          <MovieCard
+            key={ title }
+            img={ imagePath }
+            title={ title }
+            subtitle={ subtitle }
+            storyline={ storyline }
+          />
+        </div>
+      ))
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(),
+};
+
+MovieList.defaultProps = {
+  movies: [],
+};
