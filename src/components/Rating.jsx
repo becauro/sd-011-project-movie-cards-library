fcf8c18b36196b1eb85425b2ede93b2f1744bb32
Michelle <mls.srbek@gmail.com>
2021-05-27 00:04:28 -0300

    req ... destructuring correction

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,7 +6,7 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <section>{rating}</section>
+      <section className="rating">{rating}</section>
     );
   }
 }
