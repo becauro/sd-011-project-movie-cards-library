7c645d7eeab246beb51768a37a51dde2ef493401
Michelle <mls.srbek@gmail.com>
2021-05-26 23:48:35 -0300

    req ...

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,30 @@
 // implement MovieCard component here
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     // eslint-disable-next-line
-    // const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <content />
+      <content>
+        <img src={ imagePath } alt="movieImage" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={ rating } />
+      </content>
     );
   }
 }
 
-// MovieCard.propTypes = {
-//   title: PropTypes.string.isRequired,
-//   subtitle: PropTypes.string.isRequired,
-//   storyline: PropTypes.string.isRequired,
-//   rating: PropTypes.number.isRequired,
-//   imagePath: PropTypes.string.isRequired,
-// };
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  imagePath: PropTypes.string.isRequired,
+};
 
 export default MovieCard;
