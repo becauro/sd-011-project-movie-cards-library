e3c6c27a34a4420217da04e611858760956a766f
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-05-26 21:23:36 -0300

    commit bomb

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    const { movie } = this.props;
     return (
       <div className="movie-card">
-        <img className="movie-card-image" src={ imagePath } alt={ title } />
-        <div className="movie-card-body">
-          <h4 className="movie-card-title">{title}</h4>
-          <h5 className="movie-card-subtitle">{subtitle}</h5>
-          <p className="movie-card-storyline">{storyline}</p>
-        </div>
+        <img className="movie-card-image" src={ movie.imagePath } alt={ movie.title } />
+        <section className="movie-card-body">
+          <div className="movie-card-title">
+            <h4>{movie.title}</h4>
+          </div>
+          <div className="movie-card-subtitle">
+            <h5>{movie.subtitle}</h5>
+          </div>
+          <div className="movie-card-storyline">
+            <p>{movie.storyline}</p>
+          </div>
+        </section>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
 }
+
+MovieCard.propTypes = { movie: PropTypes.objectOf.isRequired };
 
 export default MovieCard;
