a52f8ade6dbd7041f38b42eef92218c4a1b5fec3
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-05-27 08:44:32 -0300

    Add styles to all Components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import '../App.css';
 
 class Header extends Component {
   render() {
     return (
-      <header>
-        <h1>Movie Cards Library</h1>
+      <header className="page-title">
+        <h1 className="movie-card-header">Movie Cards Library</h1>
       </header>
     );
   }
