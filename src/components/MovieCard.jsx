2d106e75fcff3d00be4c8fa82ae45412772564d4
lucasSCsantos <luc.cristovam10@gmail.com>
2021-05-26 21:49:09 -0300

    requisitos ate o 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,45 @@
 // implement MovieCard component here
+import React from 'react';
+import '../componentsStyles/MovieCard.css';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movies } = this.props;
+    const { title } = movies;
+    const { subtitle } = movies;
+    const { storyline } = movies;
+    const { rating } = movies;
+    const { index } = this.props;
+    const { imagePath } = movies;
+    const classes = `${index} MovieCard`;
+
+    return (
+      <div className={ classes }>
+        <div className="Image">
+          <img src={ imagePath } alt={ title } />
+        </div>
+        <div className="Infos">
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+        </div>
+        <div className="Ratings">
+          <p>{ rating }</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
+  imagePath: PropTypes.string.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+import '../componentsStyles/MovieList.css';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <main>
+        { movies.map((movie, index) => (
+          <MovieCard movies={ movie } index={ index } key={ movie.title } />
+        )) }
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
