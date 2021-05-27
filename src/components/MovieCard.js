134413f9bb53b898655e2eee661280cb1e0d3d9f
Lais Simenikim <l.simenikim@gmail.com>
2021-05-26 21:31:25 -0300

    add requisito 11, 12, 13, 14, 17

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
       <div>
+        <img src={ movie.imagePath } alt="" />
         <h4>{movie.title}</h4>
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline}</p>
-        <li>{movie.rating}</li>
-        <img src={ movie.imagePath } alt="" />
+
+        <Rating rating={ movie.rating } />
       </div>);
   }
 }
