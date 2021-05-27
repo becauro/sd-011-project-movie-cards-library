09aeb1a49438d47cfa3f913183f79f2c6ead3822
Raphael <raphaellfpg@gmail.com>
2021-05-27 19:17:56 -0300

    Linter

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
-        { movies.map((item) => <MovieCard movies = { item } key = { item.title }/>) }
+        { movies.map((item) => <MovieCard movies={ item } key={ item.title }/>) }
       </div>
     );
   }
 }
 
 export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.shape(),
+};
 
 MovieList.defaultProps = {
   movies: {},
