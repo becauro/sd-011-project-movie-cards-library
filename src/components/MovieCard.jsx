bb2ff8f0235af581d01f8de364731af7b10e6535
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-05-27 06:34:29 -0300

    aesthetic adjustments 1.0

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,21 +4,15 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: {
-      title,
-      subtitle,
-      storyline,
-      rating,
-      imagePath,
-    } } = this.props;
+    const { movie } = this.props;
 
     return (
-      <div>
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
-        <img src={ imagePath } alt="movie's art" />
+      <div className="movies">
+        <img className="images" src={ movie.imagePath } alt="movie's art" />
+        <h4>{ movie.title }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p className='story'>{ movie.storyline }</p>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
