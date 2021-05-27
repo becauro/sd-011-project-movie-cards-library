293ba743fdb9ea5c5f1eef4a74e339eff91783f6
Pedro Castro <pa.coelho.castro@gmail.com>
2021-05-27 19:40:42 -0300

    Movie Cards rendered

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-    return <div />;
+    const { movies } = this.props;
+    return (
+      movies
+        .map((singleMovie, index) => <MovieCard movie={ singleMovie } key={ index } />)
+    );
   }
 }
+
+MovieList.defaultProps = {
+  movies: [],
+};
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default MovieList;
