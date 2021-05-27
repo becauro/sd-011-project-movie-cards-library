8d090161abf7df089d9bc0a3447ac81284823074
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-05-27 09:25:23 -0300

    Terminando o projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,36 @@
 // implement MovieList component here
 import React from 'react';
-import MovieCard from '../components/MovieCard';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+// /* https://github.com/tryber/sd-011-project-movie-cards-library/pull/129/files
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
-        {movies.map((movie) => <MovieCard movie={movie} />)}
+      <div className="movie-list">
+        { movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
       </div>
     );
   }
 }
 
 export default MovieList;
+
+// /* https://github.com/tryber/sd-011-project-movie-cards-library/pull/13/files
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      imagePath: PropTypes.string,
+    }),
+  ),
+};
+
+MovieList.defaultProps = {
+  movies: [],
+};
