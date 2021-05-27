66660efcc79416403d5906f438834c33f4c0747d
nick <pereiranicolas713@gmail.com>
2021-05-27 01:30:01 -0300

    requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-//  iniciando
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>Hello World</header>
+    );
+  }
+}
+
+export default Header;
