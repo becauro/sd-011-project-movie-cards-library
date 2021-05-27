e722b0c5c252e59edc1287efa3e4c1eeaacaeb54
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-05-26 22:07:33 -0300

    Att css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ class MovieCard extends Component {
   render() { 
     const {title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return ( 
-      <div>
+      <div className='movie-card'>
         <img src={imagePath} alt={title} />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
