4d953707bf17ec5d9803f36915401d9874cab589
DADA <davimarque2000@gmail.com>
2021-05-27 13:21:28 -0300

    Requisitos 1 e 2, Header h1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import header from './components/Header';
 
 function App() {
   return (
-    <div className="App">a
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+    <div className="App">
+      <header />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
 // implement Header component here
+import React from 'react';
+
+class header extends React.Component {
+  render() {
+    const headerx = (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>);
+    return headerx;
+  }
+}
+
+export default header;
