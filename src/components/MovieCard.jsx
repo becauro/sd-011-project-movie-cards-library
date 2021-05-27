fcd24bf5a0c4b1e5fe966ec1ce151ac143c562ed
marlon307 <marlon-ramosb@hotmail.com>
2021-05-27 16:20:09 -0300

    update teste

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,11 +9,11 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <div className="movie-card-body">
-          <h4 className="movie-card-title">{ title }</h4>
-          <h5 className="movie-card-subtitle">{ subtitle }</h5>
-          <p className="movie-card-storyline">{ storyline }</p>
-          <img className="movie-card-image" src={ imagePath } alt={ title } />
-          <Rating rating={ rating } />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <img className="movie-card-image" src={imagePath} alt={title} />
+          <Rating rating={rating} />
         </div>
       </div>
     );
