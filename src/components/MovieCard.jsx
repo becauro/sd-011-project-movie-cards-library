ff35b076d67f32af03d56dedaec84d4095fb6865
Hugo Mouto <hmoutol@gmail.com>
2021-05-27 19:26:50 -0300

    feat: Todos Critérios

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 export default class MovieCard extends Component {
   render() {
-    const { movie: { title, imagePath, subtitle, storyline } } = this.props;
+    const { movie: { title, imagePath, subtitle, storyline, rating } } = this.props;
 
     return (
-      <div>
+      <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
+        <div className="movie-card-rating"><Rating rating={ rating } /></div>
       </div>
     );
   }
@@ -22,5 +24,6 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.string.isRequired,
   }).isRequired,
 };
