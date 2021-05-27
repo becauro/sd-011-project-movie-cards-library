7f8f0403afba9de8307df05f1c39fbdbe2b0d591
camiloLelis <lafirmafuerte@gmail.com>
2021-05-26 23:12:26 -0300

    requisito3-tentativa-4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -8,10 +8,16 @@ class Movielist extends React.Component {
     const { movies } = this.props;
     return (
       <div>
-        { movies.map((movie) => <MovieCard key={ movie.title} movie={ movie } />) }
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
 }
 
+MovieList.propTypes = {
+    movies: PropTypes.arrayOf(
+      PropTypes.object,
+    ).isRequired,
+  };
+  
 export default Movielist;
