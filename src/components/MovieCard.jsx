ed8fcd7dc77f75d08170838a504c17acc5cdf4f5
Daniel Leon <daniel.radialista@gmail.com>
2021-05-27 13:24:52 -0300

    requisitos 11 ao 17

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <div>
+      <div className="movie-card">
         <img src={ imagePath } alt={ title } />
-        <div>
+        <div className="movie-card-body">
           <h4>{title}</h4>
           <h5>{subtitle}</h5>
           <p>{storyline}</p>
         </div>
+        <Rating rating={ rating } />
       </div>
     );
   }
@@ -24,6 +26,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,12 @@
-// implement MovieList component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
+      <div className="movie-list">
         {movies.map((movie) => (
           <MovieCard movie={ movie } key={ movie.title } />
         ))}
