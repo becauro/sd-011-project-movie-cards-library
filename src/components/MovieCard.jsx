6f24fc9b07597b562f4f2d4012c198e2de6f6efc
Gustavo Mendes <gusm217@gmail.com>
2021-05-27 11:46:52 -0300

    Componentes estilizados

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,12 +8,12 @@ class MovieCard extends React.Component {
       movie: { title, subtitle, storyline, imagePath, rating },
     } = this.props;
     return (
-      <div>
-        <img src={ imagePath } alt="" />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={ rating } />
+      <div className="movie-card movie-card-body">
+        <img className="movie-card-image" src={ imagePath } alt="" />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <div className="movie-card-rating"><Rating rating={ rating } /></div>
       </div>
     );
   }
