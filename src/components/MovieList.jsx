c141582e6b10388ea75ca49c69e7f7de30b4f04d
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-05-28 02:34:24 -0400

    Requisito 4, feito.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-
+    const { movies } = this.props
+    return (
+      <div>
+        {movies.map((value) => <MovieCard movie={value} />)}
+      </div>
+    )    
   }
 }
 
