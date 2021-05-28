b1a066ab942a967895c5e4f8476d23bc18133d81
Gustavo Miyagawa <miyagawasantos@hotmail.com>
2021-05-28 03:28:39 -0400

    Requisito 17, feito.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props
+    const { movies } = this.props;
     return (
-      <div className='movie-list'>
-        {movies.map((value) => <MovieCard movie={value} key={value.title}/>)}
+      <div className="movie-list">
+        {movies.map((value) => <MovieCard movie={ value } key={ value.title } />)}
       </div>
-    )    
+    );
   }
 }
 
-export default MovieList
+export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
