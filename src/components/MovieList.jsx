5a1a18067d852379b68feb781f602a2d2f095ac4
nandovbr <nandovbr15@hotmail.com>
2021-05-28 13:00:44 -0300

    mais requisitos após 3 iniciais

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React, { Component } from "react";
+import MovieCard from "./MovieCard";
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      movies.map((movieItem) => <MovieCard movie={movieItem} key={movieItem.title} />)
+    );
+  }
+}
+
+export default MovieList;
