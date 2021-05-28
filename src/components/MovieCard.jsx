f5d3cee19d641fc77badc16fb6c807f821e37336
IgorMendez <igor.emp@hotmail.com>
2021-05-27 21:12:36 -0300

    Exercício 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,9 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
-      <div></div>
+      <div>
+        <img src={ imagePath } />
+      </div>
     )
   }
 }
