41a658ca9021142e155495536f2ea26751e0c590
André Dantas <aluizcfd@gmail.com>
2021-05-28 01:20:03 -0300

    MovieCard and MovieList components almost finished, PropTypes verification added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 import MovieList from './components/MovieList';
 import MovieData from './data';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MovieList movies={ MovieData } />
     </div>
   );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
-        Placeholder
-        <MovieCard />
+        {movies.map((item) => <MovieCard movie={ item } key={ item.title } />)}
       </div>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
