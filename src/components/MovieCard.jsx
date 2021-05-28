7641b7d0cf310c9405ffb8665784a9432fd744dc
Savio Moraes <smmsavio@hotmail.com>
2021-05-28 08:48:50 -0300

    riniciando projeto apos algumas tentativas de mostrar os filmes na pagina

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,41 +1,8 @@
-// implement MovieCard component here
 import React from 'react';
-
-// const rating = 4.5;
-// const ratingThree = 3;
-// const movies = [
-//   {
-//     title: 'Movie Title 1',
-//     subtitle: 'Movie Subtitle 1',
-//     storyline: 'Movie Storyline 1',
-//     rating,
-//     imagePath: 'images/movie_1',
-//   },
-//   {
-//     title: 'Movie Title 2',
-//     subtitle: 'Movie Subtitle 2',
-//     storyline: 'Movie Storyline 2',
-//     rating,
-//     imagePath: 'images/movie_2',
-//   },
-//   {
-//     title: 'Movie Title 3',
-//     subtitle: 'Movie Subtitle 3',
-//     storyline: 'Movie Storyline 3',
-//     ratingThree,
-//     imagePath: 'images/movie_3',
-//   },
-// ];
 
 class MovieCard extends React.Component {
   render() {
-    return (
-      <div>
-        <li>title1</li>
-        <li>title2</li>
-        <li>titl3</li>
-      </div>
-    );
+    return <li />;
   }
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,13 @@
 // implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
+import movies from '../data';
+
+const { movies } = this.props;
 
 class MovieList extends React.Component {
   render() {
-    return <ul><MovieCard /></ul>;
+    return <ul><MovieCard {movies} /></ul>;
   }
 }
 
