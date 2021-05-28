dffae7e3ea5ed210f20d584a307a016bef38efcf
Ana Carolina R Santos <kroline0693@gmail.com>
2021-05-28 08:40:02 -0300

    Fechando o Projetonpm run lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,31 @@
-// implement MovieList component here
+import PropTypes from 'prop-types';
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        { movies.map((card) => <MovieCard movie={ card } key={ card.title } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      imagePath: PropTypes.string,
+    }),
+  ),
+};
+MovieList.defaultProps = {
+  movies: [],
+};
