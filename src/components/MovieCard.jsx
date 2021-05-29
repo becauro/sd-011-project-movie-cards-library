49f4c14a2e787a5d770cd1bc91c0b2541e49fb16
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-05-29 01:35:27 -0300

    Organizado arquivos.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Rating from './Rating';
+import PropTypes from 'prop-types';
 
 class MovieCards extends React.Component {
   render() {
-    const { movie } = this.props;
-    const { title, subtitle, storyline, rating, imagePath } = movie;
+    const { title, subtitle, storyline, rating, imagePath } = this.prop.movie;
 
     return (
       <div className="movie-card-header">
@@ -12,9 +12,19 @@ class MovieCards extends React.Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        < Rating rating={ rating }/>
+        < Rating rating={rating}/>
       </div>);
   }
 }
 
+MovieCards.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }),
+
+}
 export default MovieCards;
