4b0c79d47601fd09c498c5eb945e771ab81218a7
Rafael Schmitz <schimitaum@gmail.com>
2021-05-29 00:04:07 -0300

    ta quase

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div className="Item">
-        Tchê!
-      </div>
+      <header className="header">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
     );
   }
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,36 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt="Imagem do filme" />
+          <div className="movie-card-header">
+            <div className="movie-card-titles">
+              <h4 className="movie-card-title">{ title }</h4>
+              <h5 className="movie-card-subtitle">{ subtitle }</h5>
+            </div>
+            <Rating rate={ rating } />
+          </div>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.exact({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
+
+export default MovieCard;
