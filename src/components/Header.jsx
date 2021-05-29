d13bd2e9d0df966e788b9e1a9781407c72d69c70
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-05-29 10:23:18 -0300

    Projeto Movie Cards Library - Envio itens 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,12 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return (<div className="headText">
-      <header>
-        <h1>Movie Cards Library</h1>
-      </header>
-    </div>
+    return (
+      <div className="headText">
+        <header>
+          <h1>Movie Cards Library</h1>
+        </header>
+      </div>
     );
   }
 }
