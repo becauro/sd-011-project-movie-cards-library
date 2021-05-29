d733eed3cf2589c1c636eb1c60209655f5f0dc02
Aline Hofner <alinehofner@gmail.com>
2021-05-28 22:46:36 -0300

    MovieCard e Rating complete

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
-class Movie extends React.Component {
+class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card">
-        <img src={ imagePath } alt={ title }></img>
+        <img src={ imagePath } alt={ title } />
+        <div>
+          <h4>
+            { title }
+          </h4>
+          <h5>
+            { subtitle }
+          </h5>
+          <p>
+            { storyline }
+          </p>
+        </div>
+        <Rating rating={ rating } />
       </div>
     );
   }
 }
 
-Movie.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+  }).isRequired,
 };
 
-export default Movie;
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    return () => {
-
-    };
+    const { rating } = this.props;
+    return (<section className="rating">{ rating }</section>
+    );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
+};
+
 export default Rating;
