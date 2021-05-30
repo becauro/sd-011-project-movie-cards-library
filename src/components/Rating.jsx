72c9f85de6629a633acc80f9e5dbc0d8499799a2
camiloLelis <lafirmafuerte@gmail.com>
2021-05-30 19:13:41 -0300

    requisitos-11,12etc-tentativa-2

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,21 +3,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
-    render() {
-        const {rating} = this.props;
-        return (
-            <div className='rating'>
-                {rating}
-            </div>
-        );
-    }
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className= "rating">
+        {rating}
+      </div>
+    );
+  }
 }
 
 Rating.propTypes = {
-    rating: PropTypes.number,
+  rating: PropTypes.number,
 };
 
 Rating.defaultProps = {
-    rating:0,
+  rating:0,
 };
 export default Rating;
