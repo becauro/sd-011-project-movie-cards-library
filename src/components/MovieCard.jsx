6f9cc3c822c41572c51ddddac8cfd65690999e78
Guilherme Sucupira Oliveira <guilherme.so23@gmail.com>
2021-05-30 16:03:41 -0300

    Completed exercise 6 and 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,10 @@ import React, { component } from 'react';
 class MovieCard extends component {
   render() {
     const movie = this.props;
-
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    return (
+      <img src={ imagePath } alt={ title } />
+    );
   }
 }
 
