4a1caad02110fa0303ef6c4d9af93b0f832c4820
Gabriel <gribeiro237@gmail.com>
2021-05-30 17:46:56 -0300

    finalizando o projeto + css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Movies from './components/MovieList';
+import Data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Movies movies={ Data } />
     </div>
   );
 }
