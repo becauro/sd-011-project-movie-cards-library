74e2ae03214d8af9f87d25cc334571783ca022f5
Priscila <prihscapin@gmail.com>
2021-05-30 10:11:13 -0300

    Primeiro commit <3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
-// implement Header component here
+// import { render } from 'enzyme';
+import React from 'react';
+// import './header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+export default Header;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,35 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div className="movieCard">
+        <img src={ imagePath } alt="poster" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <div>
+          <div>
+            <Rating rating={ rating } />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
