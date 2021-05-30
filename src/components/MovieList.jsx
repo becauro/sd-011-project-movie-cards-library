352e6c690e6f1b06e52b8f5a8640d55d63ef0db8
teofilobrandao <teofiloafssbrandao@gmail.com>
2021-05-30 11:29:35 -0300

    Projeto Movie Cards Library - Envio item 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,16 @@
 // implement MovieList component here
 import React from 'react';
+import movies from '../data';
+// import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
+    const { title, subtitle, storyLine, rating, imagePath } = this.props.movies;
+
+    return (
+    <div>
+      Título: {title} - Sub-Título: {subtitle} - Sinopse: {storyLine} - Nota: {rating} - Card: {imagePath}
+    </div>);
   }
 }
 export default MovieList;
