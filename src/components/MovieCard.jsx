758eef39cef1ccdc213c3574cccd92516e5bb52b
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-30 15:57:52 -0300

    Corrige envio do arquivo MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
-/* import PropTypes from 'prop-types'; */
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
-      <div />
+      <div>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+      </div>
     );
   }
 }
 
-/* MovieCard.propTypes = {
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(
+    PropTypes.shape({
+      imagePath: PropTypes.element.isRequired,
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      storyline: PropTypes.string.isRequired,
+    }),
+  ),
+};
 
-}; */
+MovieCard.defaultProps = {
+  movie: [],
+};
 
 export default MovieCard;
