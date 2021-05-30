1eb2552fae066c6abc9bb32593094c50c79e949f
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-05-30 01:55:09 -0300

    Requisitos 4, 5 e 6 - componente MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
-// implement MovieCard component here
+import React from 'react';
+/* import PropTypes from 'prop-types'; */
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+/* MovieCard.propTypes = {
+
+}; */
+
+export default MovieCard;
