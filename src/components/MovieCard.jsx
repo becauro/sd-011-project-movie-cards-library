62ed1979fcf373b3d928a57c51d9b0e410f54525
Marcelo Araujo <almeidadearaujomarcelo@gmail.com>
2021-05-31 08:06:39 -0300

    Adiciona os componentes do MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,32 @@
 // implement MovieList component here
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    // const { movie } = this.props;
-    // const { title, subtitle, storyline, imagePath, rating } = movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
 
     return (
-      <li />
+      <li>
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <img src={ imagePath } alt={ `${title} poster` } />
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+      </li>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
