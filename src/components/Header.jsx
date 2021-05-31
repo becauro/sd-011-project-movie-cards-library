d955e9d0f571f5b5108947bea2fa2fc36b0adc81
fredmartins <fredweb22@gmail.com>
2021-05-31 18:11:29 -0300

    finalizando header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Header extends React.Component {
+class Header extends Component {
   render() {
     return (
-      <div />
+      React.createElement('header', {
+        className: 'movie-card-header',
+      },
+      React.createElement('h1', {
+        className: 'page-title',
+      }, 'Movie Cards Library'))
     );
   }
 }
