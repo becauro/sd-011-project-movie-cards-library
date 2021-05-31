3f5e3147b1e54175456b663f7d47ea78b937543d
Nikolas <nikolas.silva1990@gmail.com>
2021-05-31 17:58:23 -0300

    6 requisitos atendidos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
-// implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <h1>oi</h1>
+    );
+  }
+}
+
+export default MovieCard;
