e70b52a2502d05ac25f871ba3a8f5d401986c7ac
Nikolas <nikolas.silva1990@gmail.com>
2021-06-01 14:12:38 -0300

    Requisitos concluídos

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
-    // const { rating } = this.props;
+    const { rating } = this.props;
     return (
       <p className="rating">{ rating }</p>
     );
   }
 }
 
-// Rating.propTypes = {
-//   rating: PropTypes.number.isRequired,
-// };
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: '1',
+};
 
 export default Rating;
