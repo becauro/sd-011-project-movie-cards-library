7052622c3c35639c2069dd127ac6b2fcae870323
Diego Figueirêdo <8kball@gmail.com>
2021-05-31 23:38:19 -0300

    Header, MovieCard, MovieList e Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,37 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: {
+      title,
+      subtitle,
+      storyline,
+      imagePath,
+      rating,
+    },
+    } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt="Movie Banner" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
