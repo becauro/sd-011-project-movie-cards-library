449b5d8118870af0c586c53dda6b5261e95dd5a6
AlyssonSene <alysson_silva@outlook.com>
2021-06-01 17:56:30 -0300

    erros linter corrigidos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
 
-
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
@@ -10,10 +9,10 @@ class MovieCard extends Component {
 
     return (
       <div>
-        <img src={imagePath} alt={title} />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
         <Rating rating={ rating } />
       </div>
     );
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import MovieCard from './MovieCard';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-    <div>
-      {movies.map((movie) => <MovieCard key={movie.title} movie={ movie } />)}
-    </div>
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
     );
   }
 }
