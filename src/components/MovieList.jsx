25b206bd3abf74b209dd9e7a63c16d87383dad0f
Diogo Bontempo <diogo_bontempo@hotmail.com>
2021-06-01 14:13:43 -0300

    commitao

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((value) => <MovieCard movie={ value } key={ value.title } />)}
+      </div>);
+  }
+}
+
+export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(propTypes.object).isRequired,
+};
