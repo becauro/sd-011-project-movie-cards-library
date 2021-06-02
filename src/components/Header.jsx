f9fc99c095cf0f47d8f2764f0fcbe78dcacbd70b
Matheus Costa <mathws.costag@gmail.com>
2021-06-02 06:19:06 -0300

    resolve problema com PropTypes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
 // Iniciando o projeto 🚀
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <li className="movie-card">
+        ah
+      </li>
+    );
+  }
+}
+
+export default MovieCard;
