4d9950011bf50da384e23f53d2f491ede22ecc54
Bernardo Coelho <bernardocezario21@gmail.com>
2021-06-02 02:45:47 -0300

    Complete Execise 4 & 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
-function MovieList() {
-  return <h1>Movie List</h1>;
+// function MovieList() {
+//   return <h1>Movie List</h1>;
+// }
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie, index) => <MovieCard key={ `Movie Title ${index + 1}` } />) }
+      </div>
+    );
+  }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
