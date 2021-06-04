70b8bd5cc0aae292f84cca6fae7ce59913fcb517
thiagofox <thiagofox00@gmail.com>
2021-06-04 10:41:44 -0300

    revisao

--- src/App.js
+++ src/App.js
@@ -9,9 +9,26 @@ function App() {
     <div className="App">
       <Header />
       <MovieList movies={ Data } />
-
     </div>
   );
 }
 
 export default App;
+
+// import React from 'react';
+// import './App.css';
+// import Header from './components/Header';
+// import MovieList from './components/MovieList';
+// import Data from './data';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <MovieList movies={ Data } />
+
+//     </div>
+//   );
+// }
+
+// export default App;
