bc41f135f6ad40c90a6e0361d12d6eab791b3fe2
Júlio Mota <juliomotacampos@gmail.com>
2021-06-07 18:58:48 -0300

    Ajuste lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,7 +10,7 @@ class MovieCard extends Component {
     return (
       <div className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ `Filme ${title}` } />
-        
+
         <div className="movie-card-body">
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
