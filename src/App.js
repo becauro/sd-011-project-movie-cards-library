9a6a9590f7fd71fd09598f4a17d9753cd23c7f38
Kevin Willian <kevinwillian084@gmail.com>
2021-06-09 16:29:44 -0300

    Adiciona requisitos 15 e 16

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
+import data from './data';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import data from './data';
 
 function App() {
   return (
