b932f0f223d71c5482d8e7388b32ab3f19e1de87
Kevin Willian <kevinwillian084@gmail.com>
2021-06-09 16:07:48 -0300

    Adiciona Requisito 3, 4 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+import '../App.css';
+import PropTypes from 'prop-types';
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        {movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />)}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
