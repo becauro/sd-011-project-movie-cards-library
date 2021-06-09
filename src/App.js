cf7725e17830202144263abc5f479e30ea632ebf
Genivaldo <cgenivaldo46@gmail.com>
2021-06-08 22:01:01 -0300

    Renderizando MovieList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import MovieList from './components/MovieList';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieList />
+      </div>
+    );
+  }
 }
 
 export default App;
