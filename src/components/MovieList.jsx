a922bf13f045cba33ba468d2fa1d5d98d3523c74
Renato Bispo <renatoslvbispo@gmail.com>
2021-06-09 21:47:52 -0300

    Finish requirement 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function MovieList(props) {
+  const { movies } = props;
+  const moviesTitles = movies.map((movie, id) => <div key={ id }>{movie.title}</div>);
+
+  return (
+    <div>{moviesTitles}</div>
+  );
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
