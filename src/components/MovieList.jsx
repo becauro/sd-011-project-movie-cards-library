53ffff016b0f51a2a1abe83da03cd394bc4b2936
LuanAlexandre <luanrb19@gmail.com>
2021-06-12 13:39:55 -0400

    Dia 11.3 - projeto-movie-cards-library: requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+import Movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = Movies;
+    return <ol></ol>;
+  }
+}
+
+export default MovieList;
