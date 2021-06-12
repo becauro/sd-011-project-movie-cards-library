d7c24b1ea9be9976cd0b01eca1a204483d68cca9
LuanAlexandre <luanrb19@gmail.com>
2021-06-12 13:03:59 -0400

    Dia 11.3 - projeto-movie-cards-library: requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
 // First project at second module
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header></header>
+    );
+  }
+}
+
+export default Header;
