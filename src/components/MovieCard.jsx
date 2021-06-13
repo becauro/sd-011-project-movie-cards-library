cfdad2068254afe52d0fb71abf54c4f4a406789a
LuanAlexandre <luanrb19@gmail.com>
2021-06-13 15:06:53 -0400

    Dia 11.3 - projeto-movie-cards-library: requisito 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,12 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div></div>
+    );
+  }
+}
