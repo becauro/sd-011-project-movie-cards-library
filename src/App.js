48cc5168bc005ad3845819a1e094dab670bd9e71
LuanAlexandre <luanrb19@gmail.com>
2021-06-13 15:31:25 -0400

    Dia 11.3 - projeto-movie-cards-library: requisito 16

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import movies from './data';
+import MovieList from './components/MovieList';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ movies } />
     </div>
   );
 }
-
-export default App;
