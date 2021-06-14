72b169e9393e2d41cf1173d07adeeb11fcc642c7
pmaiamateus <pmaiamateus@gmail.com>
2021-06-13 21:14:20 -0300

    adiciona passo 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Header extends React.Component {
+class Header extends Component {
   render() {
     return (<header><h1>Movie Cards Library</h1></header>);
   }
