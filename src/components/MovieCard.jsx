92c7710e5aa2c539d34de0063c39c6612e27aa89
pmaiamateus <pmaiamateus@gmail.com>
2021-06-13 22:09:37 -0300

    adiciona rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     const text = 'movie-card-image';
     return (
       <div>
@@ -12,6 +13,7 @@ class MovieCard extends Component {
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
+        <Rating className="movie-card-rating" rating={ rating } />
       </div>
     );
   }
@@ -23,6 +25,7 @@ MovieCard.propTypes = {
     title: propTypes.string,
     subtitle: propTypes.string,
     storyline: propTypes.string,
+    rating: propTypes.number,
   }).isRequired,
 };
 
