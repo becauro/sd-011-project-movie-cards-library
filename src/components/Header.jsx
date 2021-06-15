96add8ada457d1b39fff0e5e4584d6cc8ce1ccea
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-06-14 23:46:44 -0300

    Requisitos 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
-// Iniciando o projeto 🚀
+import React from 'react';
+
+// 1 - Crie um componente <Header />
+// 2 - Renderize um texto no <Header /> (dentro de uma tag h1)
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
