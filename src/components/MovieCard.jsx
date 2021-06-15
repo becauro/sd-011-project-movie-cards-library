ae1d2d7c8dee19d892d829ded200c8a29ef290c3
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-06-15 02:54:00 -0300

    Requisitos 7-10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,34 @@
 // 6 - Crie um componente <MovieCard />
+// 7 - Renderize a imagem do filme dentro de uma tag img
+// 8 - Renderize o título do filme dentro de uma tag h4
+// 9 - Renderize o subtítulo do filme dentro de uma tag h5
+// 10 - Renderize a sinopse do filme dentro de uma tag p
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    return <div />;
+    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    return (
+      <section>
+        <img src={ imagePath } alt="" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </section>
+    );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
