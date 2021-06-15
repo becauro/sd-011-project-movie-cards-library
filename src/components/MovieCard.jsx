fa05468461b45890148b0cf50f4e7fc11933ff7a
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-06-15 02:25:07 -0300

    Requisitos 4, 5 e 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+// 6 - Crie um componente <MovieCard />
+
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,32 @@
 // 3 - Crie um componente <MovieList />
+// 4 - Renderize componentes <MovieCard /> dentro de <MovieList />
+// 5 - Passe uma key para cada <MovieCard /> renderizado (utilizar a função map) e cada componente <MovieCard /> deve receber uma prop key
 
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return <div />;
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </div>
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
