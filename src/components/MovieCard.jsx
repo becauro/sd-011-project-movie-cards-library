0bebaef1a94ff6e40690ead4651cc4b98d2959f3
Wendell Costa <wendellcosta9141@gmail.com>
2021-06-16 19:48:43 -0300

    [Requisito 11, 12, 13 e 14 - Concluidos / Eslint Corrigido / Proptypes]

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,9 +12,20 @@ class MovieCard extends React.Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </section>
     );
   }
 }
 
 export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,23 @@
-// implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div>
+        <p className="movie-card-rating">
+          <span className="rating">{ rating }</span>
+        </p>
+      </div>
+    );
+  }
+}
+
+Rating.defaultProps = {
+  rating: 0,
+};
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
