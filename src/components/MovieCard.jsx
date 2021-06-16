2de6c17c57f0edbc250ba290a2cdc07e2cd348c9
Wendell Costa <wendellcosta9141@gmail.com>
2021-06-16 18:37:08 -0300

    [Requisito 4,5 e 6 - Concluidos]

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    return (
+      <section />
+    );
+  }
+}
+
+export default MovieCard;
