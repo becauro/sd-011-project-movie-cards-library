cf80f68970cb976b3058d59ac93908d8c0461923
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-06-17 00:32:40 -0300

    Requisitos 1 ao 5 sem lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+    render() {
+        const movies = this.props;
+        return (
+            <div>
+            
+            </div>
+        );
+    }
+}
+
+export default MovieCard;
