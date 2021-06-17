56216e5f5038cb9bdfc728aca71d143a2a128bfd
GabrielLennox <meunomeelennox@gmail.com>
2021-06-17 00:39:37 -0300

    movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class MovieCard extends Component {
   render() {
+    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+
     return (
       <div>
-        <p>Movie Card</p>
+        <img src={ imagePath } alt="" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
