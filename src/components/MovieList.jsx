78b3fd9df31fa922fa071953cbfbeb69cdff210c
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-06-17 01:17:22 -0300

    Requisitos 1 ao 11 com lint - Revisado

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,9 +7,9 @@ class MovieList extends Component {
   render() {
     return (
       <div>
-        <MovieCard movie={ movie } key="Movie Title 1" />
-        <MovieCard movie={ movie } key="Movie Title 2" />
-        <MovieCard movie={ movie } key="Movie Title 3" />
+        <MovieCard key="Movie Title 1" />
+        <MovieCard key="Movie Title 2" />
+        <MovieCard key="Movie Title 3" />
       </div>
     );
   }
