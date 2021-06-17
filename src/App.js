ad3836ef53954c913005add0871eeb62a2899b9a
Anibal <anibalneto29@gmail.com>
2021-06-16 23:04:50 -0300

    Correções Requisitos 7-10

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Header from './components/Header';
 import MovieList from './components/MovieList';
 import movies from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <MovieList movies={ movies } />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+    <MovieList movies={ movies } />
+  </div>
+)
 
 export default App;
