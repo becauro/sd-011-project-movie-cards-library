40368c983a9df5794f14f4ca500a3502b685ad71
GabrielLennox <meunomeelennox@gmail.com>
2021-06-16 23:55:47 -0300

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Header extends Component {
   render() {
@@ -6,6 +6,6 @@ export default class Header extends Component {
       <header>
         <h1>Movie Cards Library</h1>
       </header>
-    )
+    );
   }
 }
